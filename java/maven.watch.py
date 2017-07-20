403bedd2cb4c2a7253b039ae8c1d47892452f7e2
Bastian Eicher <bastian@eicher.net>
2017-07-20 20:23:57 +0200

    Updated Apache Maven and added watch file

---
+++ java/maven.watch.py
@@ -0,0 +1,14 @@
+from urllib import request
+import re
+
+def convert(match):
+    return {
+        'version': match[0].replace('alpha', 'pre1').replace('beta', 'pre2'),
+        'version-original': match[0],
+        'stability': 'developer' if 'alpha' in match[0] else ('testing' if 'beta' in match[0] else 'stable'),
+        'released': match[1]
+    }
+
+data = request.urlopen('http://archive.apache.org/dist/maven/maven-3/').read()
+matches = re.findall(r'([0-9\.\-alphabeta]+)\/<\/a>\s+(....-..-..)', data.decode())
+releases = [convert(match) for match in matches]

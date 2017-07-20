5c80e918b84f387408aca391f2e29370e2644537
Bastian Eicher <bastian@eicher.net>
2017-07-20 20:32:06 +0200

    Updated Scala and added watch file

---
+++ java/scala.watch.py
@@ -0,0 +1,14 @@
+from urllib import request
+import re
+
+def convert(match):
+    return {
+        'version': match[0].replace('M', 'pre'),
+        'version-original': match[0],
+        'stability': 'testing' if 'M' in match[0] else 'stable',
+        'released': match[1]
+    }
+
+data = request.urlopen('http://www.scala-lang.org/files/archive/').read()
+matches = re.findall(r'scala-([0-9\.\-M]+)\.zip<\/a>\s+<\/td><td align="right">(....-..-..)', data.decode())
+releases = [convert(match) for match in matches]

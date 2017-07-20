4c009a0d16581ba82480a3e11d69a23174c721af
Bastian Eicher <bastian@eicher.net>
2017-07-20 09:06:21 +0200

    Added Docker Machine

---
+++ docker/machine.watch.py
@@ -0,0 +1,12 @@
+from urllib import request
+import json
+
+def convert(release):
+    return {
+        'version': release['tag_name'][1:],
+        'stability': 'testing' if release['prerelease'] else 'stable',
+        'released': release['published_at'][0:10]
+    }
+
+data = request.urlopen('https://api.github.com/repos/docker/machine/releases').read()
+releases = [convert(release) for release in json.loads(data)]

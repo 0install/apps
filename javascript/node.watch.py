9cfdd7d23653b80ed7ba1f78eb3e04dc69f45e16
Bastian Eicher <bastian@eicher.net>
2017-07-21 08:46:11 +0200

    Updated Node.js and added watch files

---
+++ javascript/node.watch.py
@@ -0,0 +1,14 @@
+from urllib import request
+import re
+from datetime import datetime
+
+def convert(match):
+    return {
+        'version': match[0],
+        'stability': 'testing' if match[1] == '' else 'stable',
+        'released': match[2]
+    }
+
+data = request.urlopen('https://nodejs.org/en/download/releases/').read()
+matches = re.findall(r'<td data-label="Version">Node.js v([0-9\.]+)<\/td>\n\s*<td data-label="LTS">([A-Za-z]*)<\/td>\n\s*<td data-label="Date"><time>(....-..-..)<\/time><\/td>', data.decode())
+releases = [convert(match) for match in matches if match[0][0] != '0']

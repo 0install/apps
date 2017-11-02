679022b9f79c55596983e8c62c934d8879e59177
Bastian Eicher <bastian@eicher.net>
2017-11-02 09:07:38 +0100

    Updated Node.js

--- javascript/node.watch.py
+++ javascript/node.watch.py
@@ -10,5 +10,5 @@ def convert(match):
     }
 
 data = request.urlopen('https://nodejs.org/en/download/releases/').read().decode('utf-8')
-matches = re.findall(r'<td data-label="Version">Node.js v([0-9\.]+)<\/td>\n\s*<td data-label="LTS">([A-Za-z]*)<\/td>\n\s*<td data-label="Date"><time>(....-..-..)<\/time><\/td>', data)
+matches = re.findall(r'<td data-label="Version">Node.js ([0-9\.]+)<\/td>\n\s*<td data-label="LTS">([A-Za-z]*)<\/td>\n\s*<td data-label="Date"><time>(....-..-..)<\/time><\/td>', data)
 releases = [convert(match) for match in matches if match[0][0] != '0']

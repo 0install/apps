49a64d0c52128f4d5addaace6aedd42c5ecf6acc
Bastian Eicher <bastian@eicher.net>
2018-01-21 20:25:03 +0100

    Updated KeePass Professional Edition

---
+++ security/keepass-pro.watch.py
@@ -0,0 +1,6 @@
+from urllib import request
+import re
+
+data = request.urlopen('https://sourceforge.net/projects/keepass/files/KeePass%202.x/').read().decode('utf-8')
+matches = re.findall(r'([0-9\.]+)</span></a></th>\n\s*<td headers="files_date_h" class="opt"><abbr title="(....-..-..)', data)
+releases = [{'version': match[0], 'released': match[1]} for match in matches if not match[0].startswith('2.0')]

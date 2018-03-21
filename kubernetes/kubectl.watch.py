d61e827189ddbcbc8cb5d9c3b11ddf838e1b4121
Bastian Eicher <bastian@eicher.net>
2018-03-21 07:58:02 +0100

    Updated kubectl

--- kubernetes/kubectl.watch.py
+++ kubernetes/kubectl.watch.py
@@ -3,7 +3,7 @@ import json
 
 def convert(release):
     original_version = release['tag_name'].strip('v')
-    version = original_version.replace('beta.', 'rc').replace('alpha.', 'pre')
+    version = original_version.replace('rc.', 'rc').replace('beta.', 'pre2-').replace('alpha.', 'pre1-')
     stability = 'testing' if release['prerelease'] else 'stable'
     released = release['published_at'][0:10]
     return {'version': version, 'original-version': original_version, 'stability': stability, 'released': released}

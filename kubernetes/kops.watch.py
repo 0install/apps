0e99780d53064aed0131a387b7511b951985e3fd
Bastian Eicher <bastian@eicher.net>
2018-04-13 17:20:20 +0200

    Added Kubernetes Operations (kops)

---
+++ kubernetes/kops.watch.py
@@ -0,0 +1,12 @@
+from urllib import request
+import json
+
+def convert(release):
+    original_version = release['tag_name']
+    version = original_version.strip('v').replace('rc.', 'rc').replace('beta.', 'pre2-').replace('beta', 'pre2-').replace('alpha.', 'pre1-').replace('alpha', 'pre1-')
+    stability = 'testing' if release['prerelease'] else 'stable'
+    released = release['published_at'][0:10]
+    return {'version': version, 'original-version': original_version, 'stability': stability, 'released': released}
+
+data = request.urlopen('https://api.github.com/repos/kubernetes/kops/releases').read().decode('utf-8')
+releases = [convert(release) for release in json.loads(data)]

ceffa29fa4c212970b141cb53505826fe1f1eca1
Bastian Eicher <bastian@eicher.net>
2018-05-14 22:26:18 +0200

    Updated helmfile

--- kubernetes/helmfile.watch.py
+++ kubernetes/helmfile.watch.py
@@ -1,10 +1,7 @@
 from urllib import request
 import json
 
-def convert(release):
-    version = release['tag_name'].strip('v')
-    released = release['published_at'][0:10]
-    return {'version': version, 'released': released}
+excluded_versions = ['v0.14.1']
 
 data = request.urlopen('https://api.github.com/repos/roboll/helmfile/releases').read().decode('utf-8')
-releases = [convert(release) for release in json.loads(data)]
+releases = [{'version': release['tag_name'].strip('v'), 'released': release['published_at'][0:10]} for release in json.loads(data) if release['tag_name'] not in excluded_versions]

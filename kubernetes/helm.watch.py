60bca7e6b68fc7ccb90c73d66becf09a9a338e6c
Bastian Eicher <bastian@eicher.net>
2018-07-04 19:28:14 +0200

    Updated Helm

--- kubernetes/helm.watch.py
+++ kubernetes/helm.watch.py
@@ -2,10 +2,11 @@ from urllib import request
 import json
 
 def convert(release):
-    version = release['tag_name'].strip('v').replace('rc.', 'rc')
+    original_version = release['tag_name'].strip('v')
+    version = original_version.replace('rc.', 'rc')
     released = release['published_at'][0:10]
     stability = 'testing' if release['prerelease'] else 'stable'
-    return {'version': version, 'stability': stability, 'released': released}
+    return {'version': version, 'original-version': original_version, 'stability': stability, 'released': released}
 
 data = request.urlopen('https://api.github.com/repos/kubernetes/helm/releases').read().decode('utf-8')
 releases = [convert(release) for release in json.loads(data)]

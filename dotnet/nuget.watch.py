9417e5c7954bafebe09ece0df718a9e2981d56ae
Bastian Eicher <bastian@eicher.net>
2018-07-20 13:53:47 +0200

    Added NuGet

---
+++ dotnet/nuget.watch.py
@@ -0,0 +1,11 @@
+from urllib import request
+import json
+
+data = request.urlopen('https://dist.nuget.org/index.json?_=1532084851900').read().decode('utf-8')
+artifacts = json.loads(data)['artifacts'][0]['versions']
+releases = [{
+    'original-version': artifact['version'],
+    'version': str.replace(artifact['version'], 'preview', 'pre'),
+    'released': artifact['releasedate'][0:10],
+    'stability': 'testing' if '-' in artifact['version'] else 'stable'
+} for artifact in artifacts if artifact['displayName'] == 'nuget.exe']

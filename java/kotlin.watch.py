b2b7e066380bbee92cabae58e5550577700b950a
Bastian Eicher <bastian@eicher.net>
2018-08-31 12:32:58 +0200

    Added Kotlin

---
+++ java/kotlin.watch.py
@@ -0,0 +1,13 @@
+from urllib import request
+import json
+
+def convert(release):
+    return {
+        'version': release['tag_name'][1:].replace('-M', '-rc').replace('-eap', '-pre'),
+        'version-original': release['tag_name'][1:],
+        'stability': 'testing' if release['prerelease'] else 'stable',
+        'released': release['published_at'][0:10]
+    }
+
+data = request.urlopen('https://api.github.com/repos/JetBrains/kotlin/releases').read().decode('utf-8')
+releases = [convert(release) for release in json.loads(data)]

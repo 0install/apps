1b5d26524eabe78568989650cf77cc89e566b1f9
Bastian Eicher <bastian@eicher.net>
2018-09-17 19:53:36 +0200

    Added Visual Studio Code

---
+++ editors/vs-code.watch.py
@@ -0,0 +1,16 @@
+from urllib import request
+import json
+import re
+
+def handle(version):
+    linux64url = request.urlopen('https://vscode-update.azurewebsites.net/' + version + '/linux-x64/stable').url
+    linux32url = request.urlopen('https://vscode-update.azurewebsites.net/' + version + '/linux-ia32/stable').url
+    return {
+        'version': version,
+        'release-id': re.search('stable/([0-9a-f]{40})/code', linux64url).group(1),
+        'linux-amd64-timestamp': re.search('-([0-9]{10})', linux64url).group(1),
+        'linux-i386-timestamp': re.search('-([0-9]{10})', linux32url).group(1)
+    }
+
+data = request.urlopen('https://api.github.com/repos/Microsoft/vscode/tags').read().decode('utf-8')
+releases = [handle(tag['name'].strip('v')) for tag in json.loads(data) if not '/' in tag['name'] and not 'GOOD' in tag['name'] and not 'BAD' in tag['name'] and not 'bad' in tag['name']]

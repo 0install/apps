dbf796318b6799fd3aadc7924e45fb3e2795bf60
Bastian Eicher <bastian@eicher.net>
2018-09-21 13:44:03 +0200

    Added Atom

---
+++ editors/atom.watch.py
@@ -0,0 +1,10 @@
+from urllib import request
+import json
+
+def convert(release):
+    version = release['tag_name'].strip('v')
+    released = release['published_at'][0:10]
+    return {'version': version, 'released': released}
+
+data = request.urlopen('https://api.github.com/repos/atom/atom/releases').read().decode('utf-8')
+releases = [convert(release) for release in json.loads(data) if not release['prerelease']]

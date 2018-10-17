aa9b012986a7e809e6332b2a2890b80bf241d95c
Bastian Eicher <bastian@eicher.net>
2018-10-18 00:10:52 +0200

    Fixed Terraform watch script

--- hashicorp/terraform.watch.py
+++ hashicorp/terraform.watch.py
@@ -2,4 +2,4 @@ from urllib import request
 import json
 
 data = request.urlopen('https://api.github.com/repos/hashicorp/terraform/releases').read().decode('utf-8')
-releases = [{'version': release['tag_name'].strip('v'), 'released': release['published_at'][0:10]} for release in json.loads(data) if not release['prerelease']]
+releases = [{'version': release['tag_name'].strip('v'), 'released': release['published_at'][0:10]} for release in json.loads(data) if not release['prerelease'] and not '-' in release['tag_name']]

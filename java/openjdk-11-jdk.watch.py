f29722b113ed879e8558955124666275c1875ff0
Bastian Eicher <bastian@eicher.net>
2018-10-18 16:19:53 +0200

    Updated OpenJDK 11

--- java/openjdk-11-jdk.watch.py
+++ java/openjdk-11-jdk.watch.py
@@ -2,5 +2,5 @@ from urllib import request
 import re
 
 data = request.urlopen('http://jdk.java.net/11/').read().decode('utf-8')
-match = re.findall(r'openjdk-([0-9\.]+)_', data)[0]
-releases = [{'version': match}]
+match = re.findall(r'\/jdk11\/([0-9\.]+)\/GPL\/openjdk-([0-9\.]+)_', data)[0]
+releases = [{'version': match[1], 'build-counter': match[0]}]

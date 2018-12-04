ec633ac667f9624d17dc6743fa8a511c6a3ef24f
Bastian Eicher <bastian@eicher.net>
2018-12-04 17:35:26 +0100

    Added Docker Application Packages

---
+++ docker/app.watch.py
@@ -0,0 +1,12 @@
+from urllib import request
+import json
+
+def convert(release):
+    return {
+        'version': release['tag_name'].strip('v'),
+        'stability': 'testing' if release['prerelease'] else 'stable',
+        'released': release['published_at'][0:10]
+    }
+
+data = request.urlopen('https://api.github.com/repos/docker/app/releases').read().decode('utf-8')
+releases = [convert(release) for release in json.loads(data) if release['tag_name'][0] == 'v']

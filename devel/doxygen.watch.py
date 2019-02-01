cd51185c8fe4fa9187e630183a69baf3c3f33b8a
Bastian Eicher <bastian@eicher.net>
2019-02-01 13:00:45 +0100

    Fixed SourceForge watch regexes

--- devel/doxygen.watch.py
+++ devel/doxygen.watch.py
@@ -2,5 +2,5 @@ from urllib import request
 import re
 
 data = request.urlopen('https://sourceforge.net/projects/doxygen/files/').read().decode('utf-8')
-matches = re.findall(r'rel-([0-9\.]+)</span></a></th>\n\s*<td headers="files_date_h" class="opt"><abbr title="(....-..-..)', data)
-releases = [{'version': match[0], 'released': match[1]} for match in matches if match[0][0:4] != '3.0.']
+matches = re.findall(r'rel-([0-9\.]+)</span></a>\n\s*</th>\n\s*<td headers="files_date_h" class="opt"><abbr title="(....-..-..)', data)
+releases = [{'version': match[0], 'released': match[1]} for match in matches]

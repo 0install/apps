ce976d1d3bd0447f10256f55177aa3e349161312
Bastian Eicher <bastian@eicher.net>
2019-03-02 13:49:54 +0100

    Updated Python watch script to match changes to website

--- python/python.watch.py
+++ python/python.watch.py
@@ -1,5 +1,8 @@
 from urllib import request
 import re
+from datetime import datetime
+
+skipped_versions = ['3.5.5rc1', '3.5.5', '3.5.6rc1', '3.5.6', '3.5.7rc1']
 
 def convert(match):
     version_full = match[0] # e.g. 3.7.2rc2
@@ -13,9 +16,14 @@ def convert(match):
         version = version_full
         version_main = version_full
 
-    released = match[1]
+    try:
+        released_parsed = datetime.strptime(match[1].replace('Sept.', 'Sep.'), '%b. %d, %Y')
+    except ValueError:
+        released_parsed = datetime.strptime(match[1], '%B %d, %Y')
+    released = datetime.strftime(released_parsed, '%Y-%m-%d')
+
     return {'version': version, 'version-full': version_full, 'version-main': version_main, 'version-minor': version_minor, 'stability': stability, 'released': released}
 
 data = request.urlopen('https://www.python.org/downloads/windows/').read().decode('utf-8')
-matches = re.findall(r'Python (3\.[5-9]\.[0-9rc]+) - (....-..-..)', data)
-releases = [convert(match) for match in matches]
+matches = re.findall(r'Python (3\.[5-9]\.[0-9rc]+) - (.*)<\/a>', data)
+releases = [convert(match) for match in matches if match[0] not in skipped_versions]

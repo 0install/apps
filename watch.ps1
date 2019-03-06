737ed156c16b73033ae389743d69b346667aa75c
Bastian Eicher <bastian@eicher.net>
2019-03-06 22:51:19 +0100

    Added automatic 0repo merging in AppVeyor

--- watch.ps1
+++ watch.ps1
@@ -1,20 +1,32 @@
 $ErrorActionPreference = "Stop"
 pushd $(Split-Path -Path $MyInvocation.MyCommand.Definition -Parent)
 
-# Ensure 0install is in the PATH
+# Set up 0repo
+if ((Split-Path $(pwd) -Leaf) -ne "feeds") {
+  throw "This Git repo must be cloned into a directory named 'feeds' in order for 0repo to work."
+}
+copy 0repo-config.py.template ..\0repo-config.py
+mkdir -Force ..\incoming | Out-Null
+copy *\*.zip ..\incoming\
+
+# Ensure 0install is in PATH
 if (!(Get-Command 0install -ErrorAction SilentlyContinue)) {
+    echo "Downloading 0install"
     mkdir -Force "$env:TEMP\zero-install" | Out-Null
     Invoke-WebRequest "https://0install.de/files/0install.exe" -OutFile "$env:TEMP\zero-install\0install.exe"
     $env:PATH = "$env:TEMP\zero-install;$env:PATH"
 }
 
-# Exclude feeds with archives that cannot be extracted correctly on Windows
-$files = (ls -Recurse -Filter *.watch.py -Exclude go-linux.watch.py,go-darwin.watch.py).FullName
-
+# Run watch scripts
+$files = (ls -Recurse -Filter *.watch.py -Exclude go-linux.watch.py,go-darwin.watch.py).FullName # Exclude feeds with archives that cannot be extracted correctly on Windows
 foreach ($file in $files) {
-    echo "Checking $file"
-    cmd /c "0install run --batch http://0install.de/feeds/0watch.xml --output _watch $file 2>&1" # Redirect stderr to stdout
-    if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
+    echo "Running $file"
+    cmd /c "0install run --batch http://0install.de/feeds/0watch.xml --output ..\incoming $file 2>&1" # Redirect stderr to stdout
+    if ($LASTEXITCODE -ne 0) {throw "Failed with exit code $LASTEXITCODE"}
 }
+
+# Merge generated feeds
+cd ..
+cmd /c "0install run --batch --not-before 0.7 http://0install.net/tools/0repo.xml 2>&1" # Ignore error due to missing archives.db
 
 popd
--- watch.sh
+++ watch.sh
@@ -2,13 +2,22 @@
 set -e
 cd `dirname $0`
 
-# Exclude feeds with MSIs and with fully templated download URLs (currently not supported by 0template on Linux)
-##FILES=$(ls */*.watch.py | grep -v gitextensions | grep -v go-windows | grep -v kotlin | grep -v node | grep -v vagrant)
+# Set up 0repo
+if [[ "$(basename $(pwd))" != "feeds" ]]; then 
+  echo "This Git repo must be cloned into a directory named 'feeds' in order for 0repo to work."; exit 1
+fi
+cp 0repo-config.py.template ../0repo-config.py
+mkdir -p ../incoming
+cp */*.zip ../incoming/
 
-# Only feeds that are not handled on Windows
-FILES="golang/go-linux.watch.py golang/go-darwin.watch.py"
-
+# Run watch scripts
+#FILES=$(ls */*.watch.py | grep -v gitextensions | grep -v go-windows | grep -v kotlin | grep -v node | grep -v vagrant) # Exclude feeds currently not supported by 0template on Linux
+FILES="golang/go-linux.watch.py golang/go-darwin.watch.py" # Only feeds not handled on Windows
 for FILE in $FILES; do
-    echo "Checking $FILE"
-    0install run http://0install.de/feeds/0watch.xml --output _watch $FILE
+    echo "Running $FILE"
+    0install run http://0install.de/feeds/0watch.xml --output ../incoming $FILE
 done
+
+# Merge generated feeds
+cd ..
+0install run --not-before 0.7 http://0install.net/tools/0repo.xml || true # Ignore error due to missing archives.db

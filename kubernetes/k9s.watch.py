930ee9c5ea567a18d9040ab831648daf863b1c96
Bastian Eicher <bastian@eicher.net>
2019-04-04 11:21:16 +0200

    Added k9s

---
+++ kubernetes/k9s.watch.py
@@ -0,0 +1,5 @@
+from urllib import request
+import json
+
+data = request.urlopen('https://api.github.com/repos/derailed/k9s/releases').read().decode('utf-8')
+releases = [{'version': release['tag_name'], 'released': release['published_at'][0:10]} for release in json.loads(data)]

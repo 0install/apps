1f1b2806ebecd0a3a71d0a94f6f43de76cd489d4
Bastian Eicher <bastian@eicher.net>
2019-04-11 01:34:40 +0200

    Updated .NET Core

--- dotnet/core-sdk.watch.py
+++ dotnet/core-sdk.watch.py
@@ -1,15 +1,13 @@
 from urllib import request
 import re
 
-skipped_versions = []
-
 def get(uri):
     return request.urlopen(request.Request(uri, headers={'User-Agent': 'Python'})).read().decode('utf-8')
 
 def handle(version, released):
     def download_uri(arch):
         download_page = get('https://www.microsoft.com/net/download/thank-you/dotnet-sdk-' + version + '-' + arch + '-binaries')
-        return re.search('<a href="(https:\/\/download\.visualstudio\.microsoft\.com\/download\/pr\/.*?)"', download_page).group(1)
+        return re.search('<a href="(https:\/\/download\..*?)"', download_page).group(1)
 
     return {
         'version': version,
@@ -17,9 +15,12 @@ def handle(version, released):
         'windows-x64-download-uri': download_uri('windows-x64'),
         'windows-x86-download-uri': download_uri('windows-x86'),
         'linux-x64-download-uri': download_uri('linux-x64'),
-        'macos-x64-download-uri': download_uri('macos-x64')
+        #'macos-x64-download-uri': download_uri('macos-x64')
     }
 
-data = get('https://www.microsoft.com/net/download/dotnet-core/2.2')
-matches = re.findall(r'<p class="mb-5">Released (....-..-..)<\/p>[\S\s]*?\s+<h3 id="sdk-([0-9\.]+)">', data, re.MULTILINE)
-releases = [handle(version=match[1], released=match[0]) for match in matches if match[1] not in skipped_versions]
+def get_releases(major_version):
+    data = get('https://www.microsoft.com/net/download/dotnet-core/' + major_version)
+    matches = re.findall(r'<p class="mb-5">Released (....-..-..)<\/p>[\S\s]*?\s+<h3 id="sdk-([0-9\.]+)">', data, re.MULTILINE)
+    return [handle(version=match[1], released=match[0]) for match in matches if match[1]]
+
+releases = get_releases('2.1') + get_releases('2.2')
--- dotnet/core.watch.py
+++ dotnet/core.watch.py
@@ -1,15 +1,13 @@
 from urllib import request
 import re
 
-skipped_versions = []
-
 def get(uri):
     return request.urlopen(request.Request(uri, headers={'User-Agent': 'Python'})).read().decode('utf-8')
 
 def handle(version, released):
     def download_uri(arch):
         download_page = get('https://www.microsoft.com/net/download/thank-you/dotnet-runtime-' + version + '-' + arch + '-binaries')
-        return re.search('<a href="(https:\/\/download\.visualstudio\.microsoft\.com\/download\/pr\/.*?)"', download_page).group(1)
+        return re.search('<a href="(https:\/\/download\..*?)"', download_page).group(1)
 
     return {
         'version': version,
@@ -20,6 +18,9 @@ def handle(version, released):
         'macos-x64-download-uri': download_uri('macos-x64')
     }
 
-data = get('https://www.microsoft.com/net/download/dotnet-core/2.2')
-matches = re.findall(r'<h2 class="h3"><text>v<\/text>([0-9\.]+)<\/h2>[\S\s]*?\s+<p class="mb-5">Released (....-..-..)<\/p>', data, re.MULTILINE)
-releases = [handle(version=match[0], released=match[1]) for match in matches if match[0] not in skipped_versions]
+def get_releases(major_version):
+    data = get('https://www.microsoft.com/net/download/dotnet-core/' + major_version)
+    matches = re.findall(r'<h2 class="h3"><text>v<\/text>([0-9\.]+)<\/h2>[\S\s]*?\s+<p class="mb-5">Released (....-..-..)<\/p>', data, re.MULTILINE)
+    return [handle(version=match[0], released=match[1]) for match in matches if match[0]]
+
+releases = get_releases('2.1') + get_releases('2.2')

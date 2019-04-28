95581187894129b3400f463f2f054efdadb11fa0
Bastian Eicher <bastian@eicher.net>
2019-04-28 11:43:54 +0200

    Updated OpenJDK
    - Added OpenJDK 12.0.1
    - Removed watch for OpenJDK 11.x due to end-of-life
    - Changed download domain from download.java.net to download.oracle.com due to SSL certificate error

--- java/openjdk-11-jdk.watch.py
+++
@@ -1,6 +0,0 @@
-from urllib import request
-import re
-
-data = request.urlopen('http://jdk.java.net/11/').read().decode('utf-8')
-match = re.findall(r'\/jdk11\/([0-9\.]+)\/GPL\/openjdk-([0-9\.]+)_', data)[0]
-releases = [{'version': match[1], 'build-counter': match[0]}]
--- java/openjdk-12-jdk.watch.py
+++ java/openjdk-12-jdk.watch.py
@@ -2,4 +2,5 @@ from urllib import request
 import re
 
 data = request.urlopen('http://jdk.java.net/12/').read().decode('utf-8')
-releases = [{'version': re.findall(r'\/jdk12\/GPL\/openjdk-([0-9\.]+)_', data)[0]}]
+matches = re.findall(r'GA\/jdk([0-9\.]+)\/([0-9a-f]+)\/12\/', data)[0]
+releases = [{'version': matches[0], 'release-id': matches[1]}]

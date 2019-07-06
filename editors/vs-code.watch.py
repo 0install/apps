a52e3be3f383c49ce0b506e487a7e99caabaec10
Bastian Eicher <bastian@eicher.net>
2019-07-06 16:35:56 +0200

    Updated Visual Studio Code

--- editors/vs-code.watch.py
+++ editors/vs-code.watch.py
@@ -4,12 +4,10 @@ import re
 
 def handle(version):
     linux64url = request.urlopen('https://vscode-update.azurewebsites.net/' + version + '/linux-x64/stable').url
-    linux32url = request.urlopen('https://vscode-update.azurewebsites.net/' + version + '/linux-ia32/stable').url
     return {
         'version': version,
         'release-id': re.search('stable/([0-9a-f]{40})/code', linux64url).group(1),
-        'linux-amd64-timestamp': re.search('-([0-9]{10})', linux64url).group(1),
-        'linux-i386-timestamp': re.search('-([0-9]{10})', linux32url).group(1)
+        'linux-amd64-timestamp': re.search('-([0-9]{10})', linux64url).group(1)
     }
 
 data = request.urlopen('https://api.github.com/repos/Microsoft/vscode/tags').read().decode('utf-8')

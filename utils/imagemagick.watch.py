dd092d206f7a6d4b14918a74d41ded1b00ba02fe
Bastian Eicher <bastian@eicher.net>
2019-07-21 18:15:48 +0200

    Improved ImageMagick feed
    - Fixed icon URLs
    - Replaced interface URI with <feed-for> in template
    - Replace bundled ffmpeg for 64-bit builds as well (for consistency)
    - Removed download links to imagemagick.org, because archives there get removed as soon as a new version is released
    - Set stability to stable
    - Added more versions
    - Tweaked formatting

--- utils/imagemagick.watch.py
+++ utils/imagemagick.watch.py
@@ -1,8 +1,9 @@
 from urllib import request
 import json
 import re
+from datetime import datetime
 
-data = request.urlopen(request.Request('https://imagemagick.org/download/binaries/')).read().decode('utf-8')
+data = request.urlopen(request.Request('http://ftp.icm.edu.pl/packages/ImageMagick/binaries/')).read().decode('utf-8')
 						
-matches =re.findall(r'ImageMagick-([-0-9\.]+)-portable-Q16-x86.zip">ImageMagick-[-0-9\.]+\.\.&gt;</a></td><td align="right">(....-..-..)', data)
-releases = [{'version': match[0], 'released': match[1]} for match in matches]
+matches = re.findall(r'ImageMagick-(7\.[-0-9\.]+)-portable-Q16-x86.zip<\/a><\/td><td align="right">(..-...-....)', data)
+releases = [{'version': match[0], 'released': datetime.strftime(datetime.strptime(match[1], '%d-%b-%Y'), '%Y-%m-%d')} for match in matches]

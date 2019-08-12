0ee71d9174ce7884935f51b53910051f5a311707
Bastian Eicher <bastian@eicher.net>
2019-08-12 11:29:38 +0200

    Updated Nmap
    Modified CI setup to handle extracting RPM packages

--- watch.ps1
+++ watch.ps1
@@ -18,7 +18,7 @@ mkdir -Force ..\incoming | Out-Null
 cp *\*.zip ..\incoming\
 
 # Run watch scripts
-$files = (ls -Recurse -Filter *.watch.py -Exclude go-linux.watch.py,go-darwin.watch.py).FullName # Exclude feeds with archives that cannot be extracted correctly on Windows
+$files = (ls -Recurse -Filter *.watch.py -Exclude go-linux.watch.py,go-darwin.watch.py,nmap.watch.py).FullName # Exclude feeds with archives that cannot be extracted correctly on Windows
 foreach ($file in $files) {
     echo "Running $file"
     cmd /c "0install run --batch http://0install.de/feeds/0watch.xml --output ..\incoming $file 2>&1" # Redirect stderr to stdout
--- watch.sh
+++ watch.sh
@@ -12,7 +12,7 @@ cp */*.zip ../incoming/
 
 # Run watch scripts
 #FILES=$(ls */*.watch.py | grep -v cmake | grep -v gitextensions | grep -v go-windows | grep -v jq | grep -v kotlin | grep -v libreoffice | grep -v node | grep -v vagrant | grep -v vlc) # Exclude feeds currently not supported by 0template on Linux
-FILES="golang/go-linux.watch.py golang/go-darwin.watch.py" # Only feeds not handled on Windows
+FILES="golang/go-linux.watch.py golang/go-darwin.watch.py security/nmap.watch.py" # Only feeds not handled on Windows
 for FILE in $FILES; do
     echo "Running $FILE"
     0install run http://0install.de/feeds/0watch.xml --output ../incoming $FILE

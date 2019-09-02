f2a9707013db6afba7306e9a38934340ad727d9a
Bastian Eicher <bastian@eicher.net>
2019-09-02 21:06:11 +0200

    Updated Helm 3.x

--- kubernetes/helm-3.watch.py
+++ kubernetes/helm-3.watch.py
@@ -3,7 +3,7 @@ import json
 
 def convert(release):
     original_version = release['tag_name'].strip('v')
-    version = original_version.replace('rc.', 'rc').replace('alpha.', 'pre')
+    version = original_version.replace('rc.', 'rc').replace('alpha.', 'pre-pre').replace('beta.', 'pre')
     released = release['published_at'][0:10]
     stability = 'developer' if 'alpha' in original_version else ('testing' if release['prerelease'] else 'stable')
     return {'version': version, 'original-version': original_version, 'stability': stability, 'released': released}

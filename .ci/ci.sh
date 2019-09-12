37f30d55857ffa977251c6dcefd21a60736966bd
Bastian Eicher <bastian@eicher.net>
2019-09-12 15:07:26 +0200

    Clone "public" branch during CI

--- .ci/ci.sh
+++ .ci/ci.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -eux
-wget --quiet http://repo.roscidus.com/archives.db
+
+git clone https://github.com/0install/repo.roscidus.com.git --single-branch --branch=public --depth=1 public || true
+cp public/archives.db .
+
+export CI=true
 0install run http://0install.net/tools/0repo.xml

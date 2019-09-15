59e1f30eace2db702848aecfade9ecca0eea03a5
Bastian Eicher <bastian@eicher.net>
2019-09-15 23:09:52 +0200

    Updated Minikube

--- kubernetes/minikube.watch.py
+++ kubernetes/minikube.watch.py
@@ -2,9 +2,11 @@ from urllib import request
 import json
 
 def convert(release):
-    version = release['tag_name'].strip('v')
+    original_version = release['tag_name'].strip('v')
+    version = original_version.replace('rc.', 'rc').replace('beta.', 'pre2-').replace('alpha.', 'pre1-')
+    stability = 'testing' if release['prerelease'] else 'stable'
     released = release['published_at'][0:10]
-    return {'version': version, 'released': released}
+    return {'version': version, 'original-version': original_version, 'stability': stability, 'released': released}
 
 data = request.urlopen('https://api.github.com/repos/kubernetes/minikube/releases').read().decode('utf-8')
 releases = [convert(release) for release in json.loads(data)]

fda5680831a03dcd32625ad2578e8e193436035a
Bastian Eicher <bastian@eicher.net>
2019-09-19 22:34:06 +0200

    Updated Notepad++

--- editors/notepad-plus-plus.watch.py
+++ editors/notepad-plus-plus.watch.py
@@ -1,13 +1,7 @@
 from urllib import request
-import xml.etree.ElementTree as ET
-from datetime import datetime
+import json
 
-def parse(data):
-    for item in ET.fromstring(data).find('channel').findall('item'):
-        title = item.find('title').text
-        pubDate = datetime.strptime(item.find('pubDate').text[5:16], '%d %b %Y')
-        if title.startswith('Notepad++ ') and title.endswith(' released'):
-            yield {'version': title[10:-9], 'released': datetime.strftime(pubDate, '%Y-%m-%d')}
+excluded_versions = ['v7.6', 'v7.6.5']
 
-data = request.urlopen('https://notepad-plus-plus.org/feed.rss').read().decode('utf-8')
-releases = list(parse(data))
+data = request.urlopen('https://api.github.com/repos/notepad-plus-plus/notepad-plus-plus/releases').read().decode('utf-8')
+releases = [{'version': release['tag_name'].strip('v'), 'released': release['published_at'][0:10]} for release in json.loads(data) if release['tag_name'] not in excluded_versions]

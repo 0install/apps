5c2216e5585dd9d792e76f0bd35740d7b693058c
Bastian Eicher <bastian@eicher.net>
2019-10-07 20:19:30 +0200

    Updated svcat

--- kubernetes/svcat.watch.py
+++ kubernetes/svcat.watch.py
@@ -4,9 +4,11 @@ import json
 excluded_versions = ['v0.1.40']
 
 def convert(release):
-    version = release['tag_name'].strip('v')
+    original_version = release['tag_name'].strip('v')
+    version = original_version.replace('rc.', 'rc').replace('beta.', 'pre-')
+    stability = 'testing' if release['prerelease'] else 'stable'
     released = release['published_at'][0:10]
-    return {'version': version, 'released': released}
+    return {'version': version, 'original-version': original_version, 'stability': stability, 'released': released}
 
 data = request.urlopen('https://api.github.com/repos/kubernetes-incubator/service-catalog/releases').read().decode('utf-8')
 releases = [convert(release) for release in json.loads(data) if not release['tag_name'] in excluded_versions]

30763bdc18527b2905ca97b4610a3ba27ed17de3
Bastian Eicher <bastian@eicher.net>
2020-01-26 23:46:38 +0100

    Added 0install tools

--- watch.ps1
+++ watch.ps1
@@ -13,6 +13,7 @@ $files = (ls -Recurse -Filter *.watch.py -Exclude go-linux.watch.py,go-darwin.wa
 # Run watch scripts
 foreach ($file in $files) {
     echo "Running $file"
+    #cmd /c "0install run --batch https://apps.0install.net/0install/0watch.xml --output ..\incoming $file 2>&1" # Redirect stderr to stdout
     cmd /c "0install run --batch 0watch-windows.xml.selections --output ..\incoming $file 2>&1" # Redirect stderr to stdout
 }
 

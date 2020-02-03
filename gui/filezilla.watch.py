8887a7349a5cf944f0d7f081958050470dc3667a
Bastian Eicher <bastian@eicher.net>
2020-02-03 17:53:41 +0100

    Simplified and fixed FileZilla feed

--- gui/filezilla.watch.py
+++ gui/filezilla.watch.py
@@ -3,4 +3,4 @@ import re
 
 data = request.urlopen('https://filezilla-project.org/versions.php').read().decode('utf-8')
 matches = re.findall(r'<h3>([0-9\.]+) \((....-..-..)\)</h3>', data)
-releases = [{'version': match[0], 'released': match[1]} for match in matches if match[0][0:4] != '3.0.']
+releases = [{'version': match[0], 'released': match[1]} for match in matches if int(match[1][0:4]) >= 2019]

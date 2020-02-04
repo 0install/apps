d26ee84d803c642c24a91f8e61a9182469d5f05b
Bastian Eicher <bastian@eicher.net>
2020-02-04 13:00:57 +0100

    Updated Kotlin

--- java/kotlin.watch.py
+++ java/kotlin.watch.py
@@ -6,7 +6,8 @@ def convert(release):
         'version': release['tag_name'][1:].replace('-M', '-rc').replace('-eap', '-pre'),
         'stability': 'testing' if release['prerelease'] else 'stable',
         'released': release['published_at'][0:10],
-        'download-url': release['assets'][0]['browser_download_url']
+        'download-url': release['assets'][0]['browser_download_url'],
+        'download-name': release['assets'][0]['name'][:-4]
     }
 
 data = request.urlopen('https://api.github.com/repos/JetBrains/kotlin/releases').read().decode('utf-8')

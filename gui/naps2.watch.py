00321844bcf33b1a90974ee1a6400f66d366abae
Bastian Eicher <bastian@eicher.net>
2020-03-21 13:13:54 +0100

    Fix stability rating for NAPS2

--- gui/naps2.watch.py
+++ gui/naps2.watch.py
@@ -5,7 +5,7 @@ def convert(release):
     original_version = release['tag_name'].strip('v')
     version = original_version.replace('b', '-pre')
     released = release['published_at'][0:10]
-    stability = 'testing' if 'b' not in original_version else 'stable'
+    stability = 'testing' if 'b' in original_version else 'stable'
     return {'version': version, 'original-version': original_version, 'stability': stability, 'released': released}
 
 data = request.urlopen('https://api.github.com/repos/cyanfish/naps2/releases').read().decode('utf-8')

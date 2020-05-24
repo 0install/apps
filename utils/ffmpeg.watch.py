29f2462fba7f804127a300723c84a0369df910c2
Bastian Eicher <bastian@eicher.net>
2020-05-24 14:00:46 +0200

    Updated FFmpeg watch script

--- utils/ffmpeg.watch.py
+++ utils/ffmpeg.watch.py
@@ -3,5 +3,5 @@ import re
 from datetime import datetime
 
 data = urlopen(Request('https://ffmpeg.zeranoe.com/builds/win64/static/', headers={'User-Agent': 'Mozilla/5.0'})).read().decode('utf-8')
-matches = re.findall(r'ffmpeg-([0-9\.]+)-win64-static.zip<\/a><\/td><td>.*<\/td><td>(....-...-..)', data)
-releases = [{'version': match[0], 'released': datetime.strftime(datetime.strptime(match[1], '%Y-%b-%d'), '%Y-%m-%d')} for match in matches]
+matches = re.findall(r'ffmpeg-([0-9\.]+)-win64-static\.zip<\/a>\s*(..-...-....)', data)
+releases = [{'version': match[0], 'released': datetime.strftime(datetime.strptime(match[1], '%d-%b-%Y'), '%Y-%m-%d')} for match in matches]

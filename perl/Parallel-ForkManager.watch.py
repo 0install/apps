b695685c1d96a71084d957894f21511df9c7f58a
Philip Miess <Phil_Miess@yahoo.com>
2020-05-29 22:25:03 -0400

    Parallel-ForkManager add template and 0watch file

---
+++ perl/Parallel-ForkManager.watch.py
@@ -0,0 +1,15 @@
+from urllib import request
+import json
+
+excluded_versions = []
+# "https://api.github.com/repos/dluxhu/perl-parallel-forkmanager/releases
+#  'https://fastapi.metacpan.org/v1/release/v1/release/Parallel-ForkManager'
+data = request.urlopen('https://fastapi.metacpan.org/v1/release/Parallel-ForkManager').read().decode('utf-8')
+release = json.loads(data)
+releases = [{   'version': release['version'], 
+                'released': release['date'].split('T')[0], 
+                'name': release['distribution'], 
+                'summary': release['abstract'], 
+                'url': release['download_url'],
+                'creator': release['author']
+            }]

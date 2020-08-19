3b74491753b2057eefb25c80869f86e8e10f916a
Bastian Eicher <bastian@eicher.net>
2020-08-19 11:29:31 +0200

    Updated AWS CLI

--- devel/aws-cli.watch.py
+++ devel/aws-cli.watch.py
@@ -5,4 +5,4 @@ import json
 data = request.urlopen('https://api.github.com/repos/aws/aws-cli/tags').read().decode('utf-8')
 releases = [{
     'version': release['name']
-} for release in json.loads(data) if release['name'][0:2] == '2.' and not 'dev' in release['name']]
+} for release in json.loads(data) if release['name'][0:2] == '2.' and release['name'] != '2.0.32' and not 'dev' in release['name']]

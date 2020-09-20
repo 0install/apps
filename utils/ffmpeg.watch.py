e08e76663f0a5e4261d35025c38dfe98949a61e4
Bastian Eicher <bastian@eicher.net>
2020-09-20 20:02:49 +0200

    Replaced discontinued ffmpeg hosting

--- utils/ffmpeg.watch.py
+++
@@ -1,7 +0,0 @@
-from urllib.request import Request, urlopen
-import re
-from datetime import datetime
-
-data = urlopen(Request('https://ffmpeg.zeranoe.com/builds/win64/static/', headers={'User-Agent': 'Mozilla/5.0'})).read().decode('utf-8')
-matches = re.findall(r'ffmpeg-([0-9\.]+)-win64-static\.zip<\/a>\s*(..-...-....)', data)
-releases = [{'version': match[0], 'released': datetime.strftime(datetime.strptime(match[1], '%d-%b-%Y'), '%Y-%m-%d')} for match in matches]

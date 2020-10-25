be897d0b0f9d5430e8ae324bbcd7a733d585038b
Bastian Eicher <bastian@eicher.net>
2020-10-25 10:17:43 +0100

    Automatic updates

--- utils/youtube-dl.watch.py
+++
@@ -1,5 +0,0 @@
-from urllib import request
-import json
-
-data = request.urlopen('https://api.github.com/repos/rg3/youtube-dl/releases').read().decode('utf-8')
-releases = [{'version': release['tag_name'].replace('.0', '.'), 'version-original': release['tag_name'], 'released': release['published_at'][0:10]} for release in json.loads(data) if len(release['assets']) > 0]

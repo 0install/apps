92016fb12c0ca6dbc27f43100887742c31022a41
Bastian Eicher <bastian@eicher.net>
2020-11-03 03:39:24 +0100

    Update Kotlin watch script

--- java/kotlin.watch.py
+++ java/kotlin.watch.py
@@ -6,7 +6,7 @@ skipped_versions = ['v1.3.70-eap-42']
 
 def convert(release):
     return {
-        'version': release['tag_name'][1:].replace('-M', '-rc').replace('-eap', '-pre'),
+        'version': release['tag_name'][1:].replace('-RC', '-rc').replace('-M', '-rc').replace('-eap', '-pre'),
         'stability': 'testing' if release['prerelease'] else 'stable',
         'released': release['published_at'][0:10],
         'download-url': [asset['browser_download_url'] for asset in release['assets'] if asset['name'].startswith('kotlin-compiler-')][0]

cd392a342e6f009628f7cb122da273f3d72e9345
Bastian Eicher <bastian@eicher.net>
2020-12-10 09:53:54 +0100

    Fixed Shotcut watch scripts

--- gui/shotcut-linux.watch.py
+++ gui/shotcut-linux.watch.py
@@ -4,8 +4,9 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from github import releases
 
 releases = [{
-    'version': release['tag_name'].strip('v'),
-    'version-short': release['tag_name'].strip('v').replace('.', ''),
+    'tag': release['tag_name'],
+    'version': release['tag_name'].strip('v').replace('.0', '.'),
+    'date': release['tag_name'].strip('v').replace('.', ''),
     'stability': 'testing' if release['prerelease'] else 'stable',
     'released': release['published_at'][0:10]
 } for release in releases('mltframework/shotcut')]

64b54373536757c01e449e27d0e0783cce8158d7
Bastian Eicher <bastian@eicher.net>
2020-12-11 13:17:41 +0100

    Tweaked Robo 3T watch script

--- gui/robo3t.watch.py
+++ gui/robo3t.watch.py
@@ -7,4 +7,4 @@ releases = [{
     'version': release['tag_name'].strip('v'),
     'released': release['published_at'][0:10],
     'commit': re.search(r'-([0-9a-f]{7})\.', release['assets'][0]['name']).group(1)
-} for release in releases('Studio3T/robomongo') if not release['prerelease'] and release['tag_name'] != 'v1.1.1' and any(str.startswith(asset['name'], 'robo3t') for asset in release['assets'])]
+} for release in releases('Studio3T/robomongo') if not release['prerelease'] and 'beta' not in release['tag_name'] and release['tag_name'] != 'v1.1.1' and any(str.startswith(asset['name'], 'robo3t') for asset in release['assets'])]

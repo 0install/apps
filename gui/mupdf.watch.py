b9167473a1ec5cae6017f435dd9d08298d369db4
Philip Miess <Phil_Miess@yahoo.com>
2021-01-22 17:41:08 -0500

    improve stye of mupdf 0watch script

--- gui/mupdf.watch.py
+++ gui/mupdf.watch.py
@@ -1,19 +1,24 @@
-from urllib import request
-import json
+''' 0watch script for mupdf '''
 import re
-import datetime
+from urllib import request
+from datetime import datetime
 
-data = request.urlopen(request.Request('https://www.mupdf.com/downloads/index.html')).read().decode('utf-8')
-						
-matches =re.findall(r'>mupdf-([0-9\.]+)-windows.zip<', data)
-data = request.urlopen(request.Request('https://www.mupdf.com/release_history.html')).read().decode('utf-8')
 releases = []
-for amatch in matches:
-  date = datetime.datetime.today().date().isoformat()
-  secondMatch = re.search(rf'MuPDF {amatch} \((....-..-..)\)',data)
-  if secondMatch:
-    date = secondMatch[1]
+REGEX = r'>mupdf-([0-9\.]+)-windows.zip<'
+HOME = 'https://www.mupdf.com/downloads/index.html'
+HIST = 'https://www.mupdf.com/release_history.html'
+today = datetime.datetime.today().date().isoformat()
 
-  releases.append({'version': amatch,'released': date })
-    
-releases
+matches = re.findall(
+    REGEX,
+    request.urlopen(request.Request(HOME)).read().decode('utf-8')
+)
+release_history = request.urlopen(request.Request(HIST)).read().decode('utf-8')
+
+for match in matches:
+    date_regex = rf'MuPDF {match} \((....-..-..)\)'
+    release_match = re.search(date_regex, release_history)
+    releases.append({
+        'version': match,
+        'released': release_match[1] if release_match else today
+    })

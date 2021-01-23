96eb774a761a8290e1eba6f0b4586efdfa403d6a
Bastian Eicher <bastian@eicher.net>
2021-01-23 11:44:33 +0100

    Tweaked formatting of MuPDF feed and watch script

--- gui/mupdf.watch.py
+++ gui/mupdf.watch.py
@@ -1,25 +1,17 @@
-''' 0watch script for mupdf '''
 import re
 from urllib import request
 from datetime import datetime
 
-releases = []
-REGEX = r'>mupdf-([0-9\.]+)-windows.zip<'
-HOME = 'https://www.mupdf.com/downloads/index.html'
-HIST = 'https://www.mupdf.com/release_history.html'
+downloads = request.urlopen(request.Request('https://www.mupdf.com/downloads/index.html')).read().decode('utf-8')
+matches = re.findall(r'>mupdf-([0-9\.]+)-windows.zip<', downloads)
+
+release_history = request.urlopen(request.Request('https://www.mupdf.com/release_history.html')).read().decode('utf-8')
 today = datetime.today().date().isoformat()
 
-matches = re.findall(
-    REGEX,
-    request.urlopen(request.Request(HOME)).read().decode('utf-8')
-)
-release_history = request.urlopen(request.Request(HIST)).read().decode('utf-8')
-
+releases = []
 for match in matches:
-    date_regex = rf'MuPDF {match} \((....-..-..)\)'
-    release_match = re.search(date_regex, release_history)
+    release_match = re.search(rf'MuPDF {match} \((....-..-..)\)', release_history)
     releases.append({
         'version': match,
         'released': release_match[1] if release_match else today
     })
-releases

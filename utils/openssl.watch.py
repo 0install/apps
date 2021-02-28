e836f39e354d9a3b36ef8730d1c2b06e191a0a1c
Bastian Eicher <bastian@eicher.net>
2021-02-28 15:35:27 +0100

    Replaced OpenSSL Windows binary source

--- utils/openssl.watch.py
+++ utils/openssl.watch.py
@@ -1,10 +1,11 @@
 from urllib import request
-import json
+import re
 
-data = json.loads(request.urlopen('https://api.bintray.com/packages/vszakats/generic/openssl').read().decode('utf-8'))
+data = request.urlopen('http://wiki.overbyte.eu/wiki/index.php/ICS_Download').read().decode('utf-8')
+matches = re.findall(r'<td>(....-..-..)<\/td>\n<td><a rel="nofollow" class="external text" href="http:\/\/wiki\.overbyte\.eu\/arch\/openssl-([0-9\.]+[a-z]?)-win64\.zip', data)
 releases = [{
     # Convert OpenSSL version number (e.g., 1.1.0b) into Zero Install version number (e.g., 1.1.0-2)
-    'version': version[0:5] + "-" + str(ord(version[5]) - 96) if len(version) == 6 else version,
-    'version-original': version,
-    'released': data['updated'][0:10]
-} for version in data['versions']]
+    'version': match[1][0:5] + "-" + str(ord(match[1][5]) - 96) if len(match[1]) == 6 else match[1],
+    'version-original': match[1],
+    'released': match[0]
+} for match in matches]

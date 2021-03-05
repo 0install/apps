bc83a5af621b62543953d375f0ec12df7f30ef1d
Bastian Eicher <bastian@eicher.net>
2021-03-05 12:31:41 +0100

    Updated Visual Studio Code

--- gui/vs-code.watch.py
+++ gui/vs-code.watch.py
@@ -1,7 +1,8 @@
 import sys, os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from github import tags
+from github import releases
 
 releases = [{
-    'version': tag['name'].strip('v')
-} for tag in tags('Microsoft/vscode') if not '/' in tag['name'] and not 'GOOD' in tag['name'] and not 'BAD' in tag['name'] and not 'bad' in tag['name'] and not 'conflict' in tag['name'] and not 'vsda' in tag['name'] and tag['name'] != '1.999.0']
+    'version': release['tag_name'].strip('v'),
+    'released': release['published_at'][0:10]
+} for release in releases('microsoft/vscode')]

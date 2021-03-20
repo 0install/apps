71838ea76a72e5d9bdce90623163fe50ab0b3e7d
Bastian Eicher <bastian@eicher.net>
2021-03-20 11:25:45 +0100

    Removed watch script for AWS CLI

--- devel/aws-cli.watch.py
+++
@@ -1,8 +0,0 @@
-#os=Windows
-import sys, os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from github import tags
-
-releases = [{
-    'version': tag['name']
-} for tag in tags('aws/aws-cli') if tag['name'][0:2] == '2.' and tag['name'] != '2.1.29' and tag['name'] != '2.1.30' and not 'dev' in tag['name']]

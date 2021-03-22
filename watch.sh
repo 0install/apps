7d00df8abd9dbd974e9e55a10d64d47328bb365b
Bastian Eicher <bastian@eicher.net>
2021-03-22 10:31:16 +0100

    Updated 0install tools used in worklfows

--- watch.sh
+++ watch.sh
@@ -10,6 +10,6 @@ for FILE in $(ls */*.watch.py); do
     #if [[ $header == "#os=$(uname)" ]] || [[ $header != \#os=* ]]; then
     if [[ $header == "#os=$(uname)" ]]; then
         echo $FILE
-        ./0install.sh run --version-for=http://0install.net/2007/interfaces/ZeroInstall.xml 2.3.12.. https://apps.0install.net/0install/0watch.xml --output ../incoming $FILE
+        ./0install.sh run https://apps.0install.net/0install/0watch.xml --output ../incoming $FILE
     fi
 done

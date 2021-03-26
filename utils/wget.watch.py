753484b8a40d4615221f8e4d8581585f4f70ff5b
Bastian Eicher <bastian@eicher.net>
2021-03-26 12:06:05 +0100

    Updated wget

--- utils/wget.watch.py
+++ utils/wget.watch.py
@@ -3,5 +3,5 @@ import json
 import re
 
 data = request.urlopen(request.Request('https://eternallybored.org/misc/wget/releases')).read().decode('utf-8')
-matches =re.findall(r'">wget-([0-9\.]+)-win64\.zip</a>\s+(....-..-..)', data)
+matches =re.findall(r'">wget-([0-9\.-]+)-win64\.zip</a>\s+(....-..-..)', data)
 releases = [{'version': match[0], 'released': match[1]} for match in matches]

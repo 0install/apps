aa1718be87a7a01863ee0a0e48109f86bc45fe50
Bastian Eicher <bastian@eicher.net>
2021-03-27 15:54:35 +0100

    Redesigned pywin32 feed to use original .whl files

---
+++ python/pywin32.watch.py
@@ -0,0 +1,16 @@
+from urllib import request
+import json
+import re
+
+data = json.loads(request.urlopen('https://pypi.org/pypi/pywin32/json').read().decode('utf-8'))
+
+releases = []
+for version, downloads in data['releases'].items():
+    if len(downloads) == 0: continue
+    release = {
+        'version': version,
+        'released': downloads[0]['upload_time'][0:10]
+    }
+    for download in downloads:
+        release[re.findall(r'-(cp\d\dm?-win.+)\.', download['url'])[0]] = download['url']
+    releases.append(release)

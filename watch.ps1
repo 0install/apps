b23dc950e3250fe41e2b534262c63fb4aa4c72b4
Bastian Eicher <bastian@eicher.net>
2021-03-27 17:38:27 +0100

    Simplified 0install tool dependencies

--- watch.ps1
+++ watch.ps1
@@ -8,8 +8,7 @@ foreach ($file in (ls -Recurse -Filter *.watch.py).FullName) {
     $header = Get-Content $file -TotalCount 1
     if (($header -eq "#os=Windows") -or (-not $header.StartsWith("#os="))) {
         echo $file
-        #.\0install.ps1 run --batch https://apps.0install.net/0install/0watch.xml --output ..\incoming $file
-        .\0install.ps1 run --batch 0watch-windows.xml.selections --output ..\incoming $file
+        .\0install.ps1 run --batch https://apps.0install.net/0install/0watch.xml --output ..\incoming $file
         if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
     }
 }

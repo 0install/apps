4b700fbda4f19e3e1e007a5c34ed05b0977879a9
apps.0install.net <webmaster@0install.net>
2021-05-28 13:53:47 +0200

    Updated Go watch scripts

--- golang/go-windows.watch.py
+++ golang/go-windows.watch.py
@@ -2,8 +2,6 @@
 from urllib import request
 import re
 
-excluded_versions = ['1', '1.0.1', '1.0.2', '1.0.3', '1.1', '1.1.1', '1.1.2', '1.2', '1.2.1']
-
 data = request.urlopen('https://golang.org/doc/devel/release.html').read().decode('utf-8')
-matches = re.findall(r'go([0-9\.]+)\n\s+\(released (..../../..)\)', data)
-releases = [{'version': match[0], 'released': match[1].replace('/', '-')} for match in matches if match[0] not in excluded_versions]
+matches = re.findall(r'go([0-9\.]+)\n\s+\(released (....-..-..)\)', data)
+releases = [{'version': match[0], 'released': match[1]} for match in matches if match[0] ]

4915c6b11a02a03956012294ac2bdc213dbd6f61
apps.0install.net <webmaster@0install.net>
2021-06-06 11:09:59 +0200

    Updated gnuplot watch script

--- utils/gnuplot.watch.py
+++ utils/gnuplot.watch.py
@@ -1,7 +1,7 @@
 from urllib import request
 import re
 
-excluded_versions = ['5.0.2', '5.2.1', '5.4.1']
+excluded_versions = ['5.0.2', '5.2.1', '5.4.1', '5.4.2']
 
 data = request.urlopen('https://sourceforge.net/projects/gnuplot/files/gnuplot/').read().decode('utf-8')
 matches = re.findall(r'([0-9\.]+)</span></a>\n\s*</th>\n\s*<td headers="files_date_h" class="opt"><abbr title="(....-..-..)', data)

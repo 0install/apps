c47b8f2cf78ce487acef796c639662fb92e55e4c
apps.0install.net <webmaster@0install.net>
2021-06-27 12:17:41 +0200

    Updated Robo 3T watch script

--- gui/robo3t.watch.py
+++ gui/robo3t.watch.py
@@ -6,5 +6,5 @@ import re
 releases = [{
     'version': release['tag_name'].strip('v'),
     'released': release['published_at'][0:10],
-    'commit': re.search(r'-([0-9a-f]{7,8})\.', release['assets'][0]['name']).group(1)
+    'commit': re.search(r'-([0-9a-f]{7,8})\.', release['assets'][-1]['name']).group(1)
 } for release in releases('Studio3T/robomongo') if not release['prerelease'] and 'beta' not in release['tag_name'] and release['tag_name'] != 'v1.1.1' and any(str.startswith(asset['name'], 'robo3t') for asset in release['assets'])]

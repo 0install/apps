dbd6bc051d06a501bccb2916b397d129b57df6da
apps.0install.net <webmaster@0install.net>
2021-07-23 16:12:36 +0200

    Removed pre-release builds from Electron feed

--- javascript/electron.watch.py
+++ javascript/electron.watch.py
@@ -5,8 +5,6 @@ from github import releases
 excluded_versions = ['v1.8.0', 'v2.1.0-unsupported.20180809']
 
 releases = [{
-    'version': release['tag_name'][1:].replace('beta.', 'pre'),
-    'version-original': release['tag_name'][1:],
-    'stability': 'testing' if release['prerelease'] else 'stable',
+    'version': release['tag_name'][1:],
     'released': release['published_at'][0:10]
-} for release in releases('electron/electron') if not release['tag_name'] in excluded_versions]
+} for release in releases('electron/electron') if not release['prerelease'] and not release['tag_name'] in excluded_versions]

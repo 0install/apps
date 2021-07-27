05c129ef7cc4367f42b3041ada193f769a748678
apps.0install.net <webmaster@0install.net>
2021-07-27 09:47:58 +0200

    Updated Audacity

--- gui/audacity.watch.py
+++ gui/audacity.watch.py
@@ -2,5 +2,5 @@ from urllib import request
 import re
 
 data = request.urlopen('https://fossies.org/windows/misc/').read().decode('utf-8')
-matches = re.search(r'>audacity-win-([0-9.]+)\.zip', data)
+matches = re.search(r'>audacity-win-([0-9.]+)-64bit\.zip', data)
 releases = [{'version': matches[1]}]

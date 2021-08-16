c4b1ebe81a02ab2276726eb5f351684270211b03
Bastian Eicher <bastian@eicher.net>
2021-08-16 12:22:09 +0200

    Updated NuGet

--- dotnet/nuget.watch.py
+++ dotnet/nuget.watch.py
@@ -4,8 +4,8 @@ import json
 data = request.urlopen('https://dist.nuget.org/index.json?_=1532084851900').read().decode('utf-8')
 artifacts = json.loads(data)['artifacts'][0]['versions']
 releases = [{
-    'original-version': artifact['version'],
-    'version': artifact['version'].replace('preview.', 'pre').replace('preview', 'pre'),
+    'original-version': artifact['version'].replace('preview.', 'preview'),
+    'version': artifact['version'].replace('preview.', 'pre'),
     'released': artifact['releasedate'][0:10],
     'stability': 'testing' if '-' in artifact['version'] else 'stable'
 } for artifact in artifacts if artifact['displayName'] == 'nuget.exe']

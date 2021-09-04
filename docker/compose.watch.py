dcfe3f90a513dcec0be28a1486491e4baa2cedf4
Bastian Eicher <bastian@eicher.net>
2021-09-04 10:54:23 +0200

    Excluded Docker Compose v2 from watch script

--- docker/compose.watch.py
+++ docker/compose.watch.py
@@ -3,7 +3,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 from github import releases
 
 releases = [{
-    'version': release['tag_name'].strip('v'),
+    'version': release['tag_name'],
     'stability': 'testing' if release['prerelease'] else 'stable',
     'released': release['published_at'][0:10]
-} for release in releases('docker/compose') if release['tag_name'] != '1.25.2-rc1']
+} for release in releases('docker/compose') if release['tag_name'][0:2] == '1.' and release['tag_name'] != '1.25.2-rc1']

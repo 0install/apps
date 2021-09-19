7ecc78bea6113fe3ad32ccb89f3e0268a12e990e
apps.0install.net <webmaster@0install.net>
2021-09-19 16:59:38 +0200

    Added gojq

---
+++ utils/gojq.watch.py
@@ -0,0 +1,9 @@
+#os=Windows
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import github
+
+releases = [{
+    'version': release['tag_name'].strip('v'),
+    'released': release['published_at'][0:10]
+} for release in github.releases('itchyny/gojq')]

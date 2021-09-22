2e2500c9deff7f17043556559b84012ed37e887e
apps.0install.net <webmaster@0install.net>
2021-09-22 13:35:10 +0200

    Split Electron into platform-specific feeds

---
+++ javascript/electron-macos.watch.py
@@ -0,0 +1,11 @@
+#os=Darwin
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import github
+
+excluded_versions = ['v1.8.0', 'v2.1.0-unsupported.20180809']
+
+releases = [{
+    'version': release['tag_name'][1:],
+    'released': release['published_at'][0:10]
+} for release in github.releases('electron/electron') if not release['prerelease'] and not release['tag_name'] in excluded_versions]
--- javascript/electron-windows.watch.py
+++ javascript/electron-windows.watch.py
@@ -1,3 +1,4 @@
+#os=Windows
 import sys, os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import github

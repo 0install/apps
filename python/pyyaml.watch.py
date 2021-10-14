d49d6b70bf647b38a5e50198acc737ec052cfac9
apps.0install.net <webmaster@0install.net>
2021-10-14 10:57:47 +0200

    Updated pyyaml

--- python/pyyaml.watch.py
+++ python/pyyaml.watch.py
@@ -3,4 +3,4 @@ import re
 
 data = request.urlopen('https://raw.githubusercontent.com/yaml/pyyaml/master/CHANGES').read().decode('utf-8')
 matches = re.findall(r'([0-9\.]+) \((....-..-..)\)', data)
-releases = [{'version': match[0].replace('.0', '.'), 'version-original': match[0], 'released': match[1]} for match in matches]
+releases = [{'version': re.sub(r"\.0(\d)", r".\1", match[0]), 'version-original': match[0], 'released': match[1]} for match in matches]

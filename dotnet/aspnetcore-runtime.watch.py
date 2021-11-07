3b68d501f1b32fca1b2f080a19571b20ebbca957
apps.0install.net <webmaster@0install.net>
2021-11-07 11:48:47 +0100

    Added ASP.NET Core Runtime and .NET Windows Desktop Runtime

---
+++ dotnet/aspnetcore-runtime.watch.py
@@ -0,0 +1,11 @@
+from urllib import request
+import json
+
+def get_releases(channel):
+    data = request.urlopen('https://raw.githubusercontent.com/dotnet/core/master/release-notes/' + channel + '/releases.json').read().decode('utf-8')
+    return [{
+        'version': release['aspnetcore-runtime']['version'],
+        'released': release['release-date']
+    } for release in json.loads(data)['releases'] if not '-' in release['release-version'] and 'aspnetcore-runtime' in release]
+
+releases = get_releases('5.0')
---
+++ dotnet/windowsdesktop-runtime.watch.py
@@ -0,0 +1,12 @@
+from urllib import request
+import json
+
+def get_releases(channel):
+    data = request.urlopen('https://raw.githubusercontent.com/dotnet/core/master/release-notes/' + channel + '/releases.json').read().decode('utf-8')
+    return [{
+        'version': release['windowsdesktop']['version'],
+        'sdk-version': release['sdk']['version'],
+        'released': release['release-date']
+    } for release in json.loads(data)['releases'] if not '-' in release['release-version'] and 'windowsdesktop' in release]
+
+releases = get_releases('5.0')

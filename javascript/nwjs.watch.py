55f750da4f0c5b6517972141e3f5e012487f5581
apps.0install.net <webmaster@0install.net>
2021-11-20 13:32:23 +0100

    Updated NW.js

--- javascript/nwjs.watch.py
+++ javascript/nwjs.watch.py
@@ -1,3 +1,4 @@
+#os=Linux
 from urllib import request
 import re
 from datetime import datetime
@@ -9,4 +10,4 @@ releases = [{
     'version-original': match[0],
     'stability': 'developer' if ('alpha' in match[0] or 'beta' in match[0]) else ('testing' if 'rc' in match[0] else 'stable'),
     'released': datetime.strftime(datetime.strptime(match[1], '%d-%b-%Y'), '%Y-%m-%d')
-} for match in matches if match[0][0:3] == '0.2']
+} for match in matches if int(match[1][-4:]) >= 2020]

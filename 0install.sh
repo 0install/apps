973675f95e81888d48599eff97cc0f8bfff4f527
apps.0install.net <webmaster@0install.net>
2022-01-18 19:37:08 +0100

    Updated 0install bootstrap script

--- 0install.sh
+++ 0install.sh
@@ -1,6 +1,21 @@
 #!/bin/sh
 set -e
 
+if [ "$#" -eq 0 ]; then
+    echo "This script runs 0install from your PATH or downloads it on-demand."
+    echo ""
+    echo "To run 0install commands without adding 0install to your PATH:"
+    echo "./0install.sh --help"
+    echo "./0install.sh COMMAND [OPTIONS]"
+    echo ""
+    echo "To install to /usr/local:"
+    echo "sudo ./0install.sh install local"
+    echo ""
+    echo "To install to your home directory:"
+    echo "./0install.sh install home"
+    exit 1
+fi
+
 download() {
     zeroinstall_release=0install-$(uname | tr '[:upper:]' '[:lower:]')-$(uname -m)-${ZEROINSTALL_VERSION:-latest}
     download_dir=~/.cache/0install.net/$zeroinstall_release
@@ -13,9 +28,15 @@ download() {
     fi
 }
 
-if command -v 0install > /dev/null 2> /dev/null; then
-    0install "$@"
-else
+if [ "$1" = "install" ]; then
     download
-    $download_dir/files/0install "$@"
+    shift 1
+    $download_dir/install.sh "$@"
+else
+    if command -v 0install > /dev/null 2> /dev/null; then
+        0install "$@"
+    else
+        download
+        $download_dir/files/0install "$@"
+    fi
 fi

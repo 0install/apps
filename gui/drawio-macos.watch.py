8b2a9f06381cbbaa620ceee84dc8e6de9e0d0b3c
apps.0install.net <webmaster@0install.net>
2022-01-31 00:06:27 +0100

    Added diagrams.net

---
+++ gui/drawio-macos.watch.py
@@ -0,0 +1,11 @@
+#os=Darwin
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import github
+
+excluded_versions = ['v15.3.7']
+
+releases = [{
+    'version': release['tag_name'].strip('v'),
+    'released': release['published_at'][0:10]
+} for release in github.releases('jgraph/drawio-desktop') if not release['prerelease'] and not release['tag_name'] in excluded_versions]

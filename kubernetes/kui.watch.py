bcdac6cbdc04bbdedcb86482c6d2f1bdace0c35a
apps.0install.net <webmaster@0install.net>
2022-02-04 12:40:04 +0100

    Updated Kui

--- kubernetes/kui.watch.py
+++ kubernetes/kui.watch.py
@@ -3,7 +3,9 @@ import sys, os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import github
 
+excluded_versions = ['v10.4.13', 'v11.1.7']
+
 releases = [{
     'version': release['tag_name'].strip('v'),
     'released': release['published_at'][0:10]
-} for release in github.releases('kubernetes-sigs/kui') if release['tag_name'] != 'v10.4.13']
+} for release in github.releases('kubernetes-sigs/kui') if release['tag_name'] not in excluded_versions]

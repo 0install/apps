7377c5d2bd444932f134dc82d87168a10c71e9a6
apps.0install.net <webmaster@0install.net>
2022-03-07 23:27:27 +0100

    Updated Shotcut

--- gui/shotcut-macos.watch.py
+++ gui/shotcut-macos.watch.py
@@ -1,12 +1,19 @@
 #os=Darwin
-import sys, os
+import sys, os, re
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import github
 
-releases = [{
-    'tag': release['tag_name'],
-    'version': release['tag_name'].strip('v').replace('.0', '.'),
-    'date': release['tag_name'].strip('v').replace('.', ''),
-    'stability': 'testing' if release['prerelease'] else 'stable',
-    'released': release['published_at'][0:10]
-} for release in github.releases('mltframework/shotcut') if not 'UNSTABLE' in release['name'] and any(str.startswith(asset['name'], 'shotcut-macos-') for asset in release['assets'])]
+releases = []
+
+for release in github.releases('mltframework/shotcut'):
+    for asset in release['assets']:
+        asset_match = re.match(r'shotcut-macos-(\d{6})\.dmg', asset['name'])
+        if asset_match:
+            releases.append({
+                'tag': release['tag_name'],
+                'version': release['tag_name'].strip('v').replace('.0', '.'),
+                'date': asset_match.group(1),
+                'stability': 'testing' if release['prerelease'] else 'stable',
+                'released': release['published_at'][0:10]
+            })
+            break
--- gui/shotcut-windows.watch.py
+++ gui/shotcut-windows.watch.py
@@ -1,12 +1,19 @@
 #os=Windows
-import sys, os
+import sys, os, re
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import github
 
-releases = [{
-    'tag': release['tag_name'],
-    'version': release['tag_name'].strip('v').replace('.0', '.'),
-    'date': release['tag_name'].strip('v').replace('.', ''),
-    'stability': 'testing' if release['prerelease'] else 'stable',
-    'released': release['published_at'][0:10]
-} for release in github.releases('mltframework/shotcut') if not 'UNSTABLE' in release['name']]
+releases = []
+
+for release in github.releases('mltframework/shotcut'):
+    for asset in release['assets']:
+        asset_match = re.match(r'shotcut-win64-(\d{6})\.zip', asset['name'])
+        if asset_match:
+            releases.append({
+                'tag': release['tag_name'],
+                'version': release['tag_name'].strip('v').replace('.0', '.'),
+                'date': asset_match.group(1),
+                'stability': 'testing' if release['prerelease'] else 'stable',
+                'released': release['published_at'][0:10]
+            })
+            break

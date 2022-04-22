3e8b9e96f36be8de5e7e12668e54345c17c74a61
apps.0install.net <webmaster@0install.net>
2022-04-22 15:51:16 +0200

    Updated Rambox

--- gui/rambox-ce.watch.py
+++ gui/rambox-ce.watch.py
@@ -1,4 +1,3 @@
-#os=Darwin
 import sys, os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import github
@@ -6,4 +5,4 @@ import github
 releases = [{
     'version': release['tag_name'],
     'released': release['published_at'][0:10]
-} for release in github.releases('ramboxapp/community-edition') if any('-mac' in asset['name'] for asset in release['assets'])]
+} for release in github.releases('ramboxapp/community-edition')]
--- gui/rambox.watch.py
+++ gui/rambox.watch.py
@@ -1,9 +1,8 @@
-#os=Windows
 import sys, os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import github
 
 releases = [{
-    'version': release['tag_name'],
+    'version': release['tag_name'].strip('v'),
     'released': release['published_at'][0:10]
-} for release in github.releases('ramboxapp/community-edition') if not release['tag_name'] in ('0.5.2', '0.5.3', '0.5.9')]
+} for release in github.releases('ramboxapp/download') if release['tag_name'][:2] != 'v1']

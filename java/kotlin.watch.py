313ac95b79d438857f571de1a47acbfeb2cfa948
apps.0install.net <webmaster@0install.net>
2022-05-07 00:46:53 +0200

    Updated Kotlin

--- java/kotlin.watch.py
+++ java/kotlin.watch.py
@@ -4,7 +4,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 import github
 
 releases = [{
-    'version': release['tag_name'][1:].replace('-RC', '-rc').replace('-M', '-rc').replace('-eap', '-pre'),
+    'version': release['tag_name'][1:].replace('-RC', '-rc').replace('-M', '-rc').replace('-eap', '-pre').replace('-Beta', '-pre-pre'),
     'stability': 'testing' if release['prerelease'] else 'stable',
     'released': release['published_at'][0:10],
     'download-url': [asset['browser_download_url'] for asset in release['assets'] if asset['name'].startswith('kotlin-compiler-')][0]

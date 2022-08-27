d21c346de343db0fba90920e3b0941be86c01600
apps.0install.net <webmaster@0install.net>
2022-08-27 22:18:02 +0200

    Use pre-built archives for .NET Windows Desktop Runtime

--- dotnet/windowsdesktop-runtime.watch.py
+++ dotnet/windowsdesktop-runtime.watch.py
@@ -5,7 +5,6 @@ def get_releases(channel):
     data = request.urlopen('https://raw.githubusercontent.com/dotnet/core/master/release-notes/' + channel + '/releases.json').read().decode('utf-8')
     return [{
         'version': release['windowsdesktop']['version'],
-        'sdk-version': release['sdk']['version'],
         'released': release['release-date']
     } for release in json.loads(data)['releases'] if not '-' in release['release-version'] and 'windowsdesktop' in release]
 

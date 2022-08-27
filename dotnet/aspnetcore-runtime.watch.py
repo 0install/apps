d8dd2ffa5a88846e545a00c55cdce8280a73c188
apps.0install.net <webmaster@0install.net>
2022-08-27 22:12:13 +0200

    Stop watching for .NET 5.0 releases

--- dotnet/aspnetcore-runtime.watch.py
+++ dotnet/aspnetcore-runtime.watch.py
@@ -8,4 +8,4 @@ def get_releases(channel):
         'released': release['release-date']
     } for release in json.loads(data)['releases'] if not '-' in release['release-version'] and 'aspnetcore-runtime' in release]
 
-releases = get_releases('5.0') + get_releases('6.0')
+releases = get_releases('6.0')

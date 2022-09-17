47112304dde194224e14693371832021b380e5b5
apps.0install.net <webmaster@0install.net>
2022-09-17 13:05:02 +0200

    Update Google API Linter watch script

--- protobuf/google-api-linter.watch.py
+++ protobuf/google-api-linter.watch.py
@@ -2,6 +2,9 @@ import sys, os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import github
 
+excluded_versions = ['v1.36.0']
+
 releases = [{
     'version': release['tag_name'].strip('v'),
-    'released': release['published_at'][0:10]} for release in github.releases('googleapis/api-linter')]
+    'released': release['published_at'][0:10]
+} for release in github.releases('googleapis/api-linter') if not release['tag_name'] in excluded_versions]

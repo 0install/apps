a304f20eee49a49316befc74688e3686f25b3398
apps.0install.net <webmaster@0install.net>
2022-11-07 10:41:47 +0100

    Updated InfluxDB Client

--- devel/influxdb-client.watch.py
+++ devel/influxdb-client.watch.py
@@ -6,4 +6,4 @@ import github
 releases = [{
     'version': release['tag_name'].strip('v'),
     'released': release['published_at'][0:10]
-} for release in github.releases('influxdata/influxdb') if not release['prerelease'] and release['tag_name'][0:2] == 'v2']
+} for release in github.releases('influxdata/influxdb') if not release['prerelease'] and release['tag_name'][0:2] == 'v2' and release['tag_name'] != 'v2.5.1']

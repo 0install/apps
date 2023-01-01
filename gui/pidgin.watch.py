3f92ffd442de995ee71cbec80a07442040851562
Bastian Eicher <bastian@eicher.net>
2023-01-01 11:32:25 +0100

    Skip Pidgin version without Windows binaries

--- gui/pidgin.watch.py
+++ gui/pidgin.watch.py
@@ -1,7 +1,7 @@
 from urllib import request
 import re
 
-excluded_versions = ['1.5.1', '2.0.0', '2.0.1', '2.0.2', '2.1.0', '2.1.1', '2.2.0', '2.2.1', '2.2.0', '2.5.9', '2.6.0', '2.13.0']
+excluded_versions = ['1.5.1', '2.0.0', '2.0.1', '2.0.2', '2.1.0', '2.1.1', '2.2.0', '2.2.1', '2.2.0', '2.5.9', '2.6.0', '2.13.0', '2.14.11']
 
 data = request.urlopen('https://sourceforge.net/projects/pidgin/files/Pidgin/').read().decode('utf-8')
 matches = re.findall(r'([0-9\.]+)</span></a>\n\s*</th>\n\s*<td headers="files_date_h" class="opt"><abbr title="(....-..-..)', data)

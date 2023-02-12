ef3dc2f6146a8df4bb250c38a9f5dba8e762d8a2
Bastian Eicher <bastian@eicher.net>
2023-02-12 12:42:00 +0100

    Retry failed watch scripts once

--- watch.ps1
+++ watch.ps1
@@ -4,12 +4,23 @@ pushd $PSScriptRoot
 mkdir -Force ..\incoming | Out-Null
 cp *\*.zip ..\incoming\
 
+function Run-WatchScript($file) {
+    echo $file
+    .\0install.ps1 run --batch https://apps.0install.net/0install/0watch.xml --output ..\incoming $file
+    if ($LASTEXITCODE -ne 0) {
+        throw "Failed with exit code $LASTEXITCODE"
+    }
+}
+
 foreach ($file in (ls -Recurse -Filter *.watch.py).FullName) {
     $header = Get-Content $file -TotalCount 1
     if (($header -eq "#os=Windows") -or (-not $header.StartsWith("#os="))) {
-        echo $file
-        .\0install.ps1 run --batch https://apps.0install.net/0install/0watch.xml --output ..\incoming $file
-        if ($LASTEXITCODE -ne 0) {throw "Exit Code: $LASTEXITCODE"}
+        try { Run-WatchScript $file }
+        catch {
+            Write-Warning "Failed, wait and retry once"
+            Start-Sleep -Seconds 10
+            Run-WatchScript $file
+        }
     }
 }
 
--- watch.sh
+++ watch.sh
@@ -5,11 +5,19 @@ cd `dirname $0`
 mkdir -p ../incoming
 cp */*.zip ../incoming/
 
+function run_watch_script() {
+    echo $FILE
+    ./0install.sh run https://apps.0install.net/0install/0watch.xml --output ../incoming $1
+}
+
 for FILE in $(ls */*.watch.py); do
     header=$(head -n 1 $FILE)
     #if [[ $header == "#os=$(uname)" ]] || [[ $header != \#os=* ]]; then
     if [[ $header == "#os=$(uname)" ]]; then
-        echo $FILE
-        ./0install.sh run https://apps.0install.net/0install/0watch.xml --output ../incoming $FILE
+        run_watch_script $FILE || {
+            echo "Failed, wait and retry once"
+            sleep 10
+            run_watch_script $FILE
+        }
     fi
 done

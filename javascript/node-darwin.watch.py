bfdf9be467ff4e1d200e24a78c94399abfef545b
Bastian Eicher <bastian@eicher.net>
2023-03-12 13:54:35 +0100

    Fixed Node.js watch scripts

--- javascript/node-darwin.watch.py
+++ javascript/node-darwin.watch.py
@@ -1,14 +1,12 @@
 #os=Darwin
 from urllib import request
-import re
-from datetime import datetime
+import json
 
-excluded_versions = ['9.11.0']
+excluded_versions = ['v9.11.0']
 
-data = request.urlopen('https://nodejs.org/en/download/releases/').read().decode('utf-8')
-matches = re.findall(r'<td data-label="Version">Node.js ([0-9\.]+)<\/td>\n\s*<td data-label="LTS">([A-Za-z]*)<\/td>\n\s*<td data-label="Date"><time>(....-..-..)<\/time><\/td>', data)
+data = request.urlopen('https://nodejs.org/dist/index.json').read().decode('utf-8')
 releases = [{
-    'version': match[0],
-    'stability': 'testing' if match[1] == '' else 'stable',
-    'released': match[2]
-} for match in matches if match[0][0] != '0' and not match[0] in excluded_versions]
+    'version': release['version'][1:],
+    'stability': 'stable' if release['lts'] else 'testing',
+    'released': release['date']
+} for release in json.loads(data) if 'osx-arm64-tar' in release['files'] and not release['version'] in excluded_versions]

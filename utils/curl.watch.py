ea0484682f1acdf956f0c72ff12d8ce3504d3ed4
Bastian Eicher <bastian@eicher.net>
2023-03-21 12:27:59 +0100

    Added cURL 8.x

--- utils/curl.watch.py
+++ utils/curl.watch.py
@@ -7,4 +7,4 @@ matches = re.findall(r'<h2> Fixed in (\d+\.\d+\.\d+) - (\w+ \d+ \d+) <\/h2>', da
 releases = [{
     'version': match[0],
     'released': datetime.strftime(datetime.strptime(match[1], '%B %d %Y'), '%Y-%m-%d')
-} for match in matches if int(match[1][-4:]) >= 2020]
+} for match in matches if int(match[1][-4:]) >= 2023]

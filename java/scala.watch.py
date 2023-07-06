658c944b4f581ce6ba62a236c553d36928486f32
Bastian Eicher <bastian@eicher.net>
2023-07-06 19:51:09 +0200

    Update scala.watch.py
    Updated Scala watch URL

--- java/scala.watch.py
+++ java/scala.watch.py
@@ -1,7 +1,7 @@
 from urllib import request
 import re
 
-data = request.urlopen('http://www.scala-lang.org/files/archive/').read().decode('utf-8')
+data = request.urlopen('https://www.scala-lang.org/files/archive/').read().decode('utf-8')
 matches = re.findall(r'scala-([0-9\.\-M]+)\.zip<\/a>\s+<\/td><td align="right">(....-..-..)', data)
 releases = [{
     'version': match[0].replace('M', 'pre'),

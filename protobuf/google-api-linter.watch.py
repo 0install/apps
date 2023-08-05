12b11a3179200d76407b54d1f17536a1bca98bba
Bastian Eicher <bastian@eicher.net>
2023-08-05 14:26:10 +0200

    Skip Google API Linter version

--- protobuf/google-api-linter.watch.py
+++ protobuf/google-api-linter.watch.py
@@ -2,7 +2,7 @@ import sys, os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import github
 
-excluded_versions = ['v1.36.0']
+excluded_versions = ['v1.36.0', 'v1.55.1']
 
 releases = [{
     'version': release['tag_name'].strip('v'),

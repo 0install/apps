2609441d59a38d57345e99e1641d3a14fec803ce
Bastian Eicher <bastian@eicher.net>
2023-08-14 18:54:22 +0200

    Added MinIO Client

---
+++ utils/minio-client.watch.py
@@ -0,0 +1,10 @@
+from urllib.request import urlopen, Request
+import re
+
+data = urlopen(Request('https://dl.min.io/client/mc/release/darwin-arm64/archive/')).read().decode('utf-8')
+matches = re.findall(r'mc\.RELEASE\.((\d\d\d\d-\d\d-\d\d)T\d\d-\d\d-\d\dZ)<', data)
+releases = [{
+    'version': match[1].replace('-', '.').replace('.0', '.'),
+    'released': match[1],
+    'timestamp': match[0]
+} for match in matches]

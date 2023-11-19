c8a5e581d87ab38c7bec2f51f5e356bda51a9c3d
Bastian Eicher <bastian@eicher.net>
2023-11-19 14:09:45 +0100

    Added macOS builds for DB Browser for SQLite

--- gui/sqlitebrowser.watch.py
+++ gui/sqlitebrowser.watch.py
@@ -1,3 +1,4 @@
+#os=Darwin
 import sys, os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import github
@@ -7,4 +8,4 @@ releases = [{
     'original-version': release['tag_name'].strip('v'), 
     'stability': 'testing' if release['prerelease'] else 'stable',
     'released': release['published_at'][0:10]
-} for release in github.releases('sqlitebrowser/sqlitebrowser') if not 'alpha' in release['tag_name'] and any(str.endswith(asset['name'], '-win64.zip') for asset in release['assets'])]
+} for release in github.releases('sqlitebrowser/sqlitebrowser') if not 'continuous' in release['tag_name'] and not 'alpha' in release['tag_name'] and any(str.endswith(asset['name'], '-win64.zip') for asset in release['assets'])]

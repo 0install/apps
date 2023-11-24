0876d813b3be4749bba84a08300cddb796d443b6
Bastian Eicher <bastian@eicher.net>
2023-11-24 13:02:49 +0100

    Fixed Blender watch script

--- gui/blender.py
+++ gui/blender.py
@@ -1,13 +1,15 @@
-from urllib import request
+from urllib.request import urlopen, Request
 import re
 from datetime import datetime
 
 def regex(url, pattern):
-    return re.findall(pattern, request.urlopen(url).read().decode('utf-8'))
+    data = urlopen(Request(url, headers={'User-Agent': 'Mozilla'})).read().decode('utf-8')
+    return re.findall(pattern, data)
 
 def releases(suffix):
     for minor_version in regex('https://download.blender.org/release/', r'>Blender([\d\.a-z]+)\/<\/a>'):
-        for match in regex('https://download.blender.org/release/Blender' + minor_version + '/', r'>blender-([\d\.\-a-z]+)-' + re.escape(suffix) + '<\/a>\s+(..-...-....)'):
+        if minor_version.startswith('1.') or minor_version.startswith('2.'): continue
+        for match in regex('https://download.blender.org/release/Blender' + minor_version + '/', r'>blender-([\d\.\-a-z]+)-' + re.escape(suffix) + r'<\/a>\s+(..-...-....)'):
             yield {
                 'version': match[0],
                 'minor-version': minor_version,

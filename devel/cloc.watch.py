6843a651ce0a4b1a0c560a8b14cea56f9b5e4a2e
Bastian Eicher <bastian@eicher.net>
2024-02-20 12:26:18 +0100

    Updated cloc watch script

--- devel/cloc.watch.py
+++ devel/cloc.watch.py
@@ -3,6 +3,7 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 import github
 
 releases = [{
-    'version': release['tag_name'].strip('v'),
+    'version': release['tag_name'].strip('v').replace('.0', '.'),
+    'original-version': release['tag_name'].strip('v'),
     'released': release['published_at'][0:10]
 } for release in github.releases('AlDanial/cloc')]

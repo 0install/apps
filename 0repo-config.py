b8e97a30d1bf8011a31f7e1991cd8a6bfd6bb3bc
Bastian Eicher <bastian@eicher.net>
2024-04-06 10:20:33 +0200

    Include 0install tools in top-level catalog

--- 0repo-config.py
+++ 0repo-config.py
@@ -155,7 +155,7 @@ def is_excluded_from_catalog(feed_root, dir_rel_path):
 	def is_executable():
 		return has_main('group') or has_main('implementation') or has_main('package-implementation') or has_command('run') or has_tag('entry-point')
 	def excluded_from_toplevel():
-		return not is_executable() or feed_root.getAttribute('uri').startswith('https://apps.0install.net/lib/') or feed_root.getAttribute('uri').endswith('runtime.xml') or feed_root.getAttribute('uri').startswith('https://apps.0install.net/dotnet/clr') or feed_root.getAttribute('uri').startswith('https://apps.0install.net/0install/')
+		return not is_executable() or feed_root.getAttribute('uri').startswith('https://apps.0install.net/lib/') or feed_root.getAttribute('uri').endswith('runtime.xml') or feed_root.getAttribute('uri').startswith('https://apps.0install.net/dotnet/clr') or feed_root.getAttribute('uri').startswith('https://apps.0install.net/0install/0install')
 
 	return has_tag('replaced-by') or has_tag('feed-for') or (dir_rel_path == '' and excluded_from_toplevel())
 

490a007e3526ec6cf358b0fc968efb5c02026ae4
Bastian Eicher <bastian@eicher.net>
2024-04-25 08:35:21 +0200

    Updated Buf

--- protobuf/buf.watch.py
+++ protobuf/buf.watch.py
@@ -3,7 +3,8 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')
 import github
 
 releases = [{
-    'version': release['tag_name'].strip('v'),
+    'version': release['tag_name'].strip('v').replace('beta.', 'pre'),
+    'tag': release['tag_name'],
     'stability': 'testing' if release['prerelease'] else 'stable',
     'released': release['published_at'][0:10]
 } for release in github.releases('bufbuild/buf') if any(str.startswith(asset['name'], 'buf-') for asset in release['assets'])]

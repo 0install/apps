a649cf7aaa3a7983cc202c5c9a472813bb074a4c
Bastian Eicher <bastian@eicher.net>
2024-07-16 09:27:37 +0200

    Updated DB Browser for SQLite

--- gui/sqlitebrowser-windows.watch.py
+++ gui/sqlitebrowser-windows.watch.py
@@ -2,12 +2,14 @@ from urllib import request
 import re
 
 releases = []
+skipped_releases = ['2024-06-15', '2024-06-16']
 
 data = request.urlopen('https://nightlies.sqlitebrowser.org/win64/').read().decode('utf-8')
 for month in re.findall(r'href="(\d{4}-\d{2})', data):
     if str.startswith(month, '2023'): continue
     data = request.urlopen('https://nightlies.sqlitebrowser.org/win64/' + month + '/').read().decode('utf-8')
     for released in re.findall(r'href="DB\.Browser\.for\.SQLite-(\d{4}-\d{2}-\d{2})-win64', data):
+        if released in skipped_releases: continue        
         releases.append({
             'month': month,
             'released': released,

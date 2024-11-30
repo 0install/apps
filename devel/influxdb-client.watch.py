4556d583d5b0274264a93687904c72ba13ea96a2
Bastian Eicher <bastian@eicher.net>
2024-11-30 18:37:28 +0100

    Updated InfluxDB download domain

--- devel/influxdb-client.watch.py
+++ devel/influxdb-client.watch.py
@@ -5,5 +5,5 @@ import re
 req = Request('https://docs.influxdata.com/influxdb/cloud/reference/cli/influx/?t=Linux')
 req.add_header('Accept-Encoding', 'gzip')
 data = gzip.decompress(urlopen(req).read()).decode('utf-8')
-matches = re.search(r'wget https:\/\/download.influxdata.com\/influxdb\/releases\/influxdb2-client-([0-9.]+)-linux-amd64\.tar\.gz', data)
+matches = re.search(r'wget https:\/\/dl.influxdata.com\/influxdb\/releases\/influxdb2-client-([0-9.]+)-linux-amd64\.tar\.gz', data)
 releases = [{'version': matches[1]}]

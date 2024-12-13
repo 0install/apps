f33b3473e57daef443fa15f1f6e19d010c8eaf5b
Bastian Eicher <bastian@eicher.net>
2024-12-13 22:18:06 +0100

    Fixed Media Player Classic - Home Cinema feed

--- gui/mpc-hc.watch.py
+++
@@ -1,9 +0,0 @@
-import sys, os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-import github
-
-excluded_versions = ['1.7.8', '1.7.12']
-
-releases = [{
-    'version': tag['name']
-} for tag in github.tags('mpc-hc/mpc-hc') if not '_' in tag['name'] and not tag['name'] in excluded_versions]

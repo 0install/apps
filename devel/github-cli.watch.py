ebe154ccc0148e87a9ea8d5bf5c86d4db6fa251f
Bastian Eicher <bastian@eicher.net>
2025-01-30 14:55:26 +0100

    Fixed GitHub CLI watch script

--- devel/github-cli.watch.py
+++ devel/github-cli.watch.py
@@ -5,4 +5,4 @@ import github
 releases = [{
     'version': release['tag_name'].strip('v'),
     'released':  release['published_at'][0:10]
-} for release in github.releases('cli/cli') if not release['prerelease']]
+} for release in github.releases('cli/cli') if not release['prerelease'] and any(asset['label'].endswith('windows arm64') for asset in release['assets'])]

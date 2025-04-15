b0e551735920873af1c1babeefcb6c7cc3914d71
Jérôme Vasseur <jerome.vasseur@jhome.fr>
2025-04-15 10:56:47 +0200

    Add VSCodium

---
+++ gui/vs-codium.watch.py
@@ -0,0 +1,8 @@
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import github
+
+releases = [{
+    'version': release['tag_name'],
+    'released': release['published_at'][0:10]
+} for release in github.releases('VSCodium/vscodium')]

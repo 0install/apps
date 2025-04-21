084c509d87bdc51094e4814e8506a1a9b5c7b65d
Bastian Eicher <bastian@eicher.net>
2025-04-21 22:43:47 +0200

    Updated "DB Browser for SQLite" watch scripts and templates

--- gui/sqlitebrowser-macos.watch.py
+++ gui/sqlitebrowser-macos.watch.py
@@ -1,4 +1,4 @@
-#os=Windows
+#os=Darwin
 import sys, os, re
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import github
@@ -7,7 +7,7 @@ releases = []
 
 for release in github.releases('sqlitebrowser/sqlitebrowser'):
     for asset in release['assets']:
-        asset_match = re.match(r'DB\.Browser\.for\.SQLite-v[\d\.]+-win64\.zip', asset['name'])
+        asset_match = re.match(r'DB\.Browser\.for\.SQLite-v[\d\.]+\.dmg', asset['name'])
         if asset_match:
             releases.append({
                 'version': release['tag_name'].strip('v'),

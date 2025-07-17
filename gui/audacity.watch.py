18c2f5417e1e25b3094203839d6aa02792b6e985
Bastian Eicher <bastian@eicher.net>
2025-07-17 20:50:59 +0200

    Changed Audacity download source to GitHub

--- gui/audacity.watch.py
+++ gui/audacity.watch.py
@@ -1,6 +1,12 @@
-from urllib import request
-import re
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import github
 
-data = request.urlopen('https://fossies.org/windows/misc/').read().decode('utf-8')
-matches = re.search(r'>audacity-win-([0-9.]+)-64bit\.zip', data)
-releases = [{'version': matches[1]}]
+def convert(release):
+    original_version = release['tag_name'][len('Audacity-'):]
+    version = original_version.replace('-beta', '-pre')
+    released = release['published_at'][0:10]
+    stability = 'testing' if release['prerelease'] else 'stable'
+    return {'version': version, 'original-version': original_version, 'stability': stability, 'released': released}
+
+releases = [convert(release) for release in github.releases('audacity/audacity') if release['assets']]

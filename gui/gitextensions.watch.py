647906dcd6b66cbc87540e57480f34976fa5cb6c
Bastian Eicher <bastian@eicher.net>
2025-11-06 16:28:14 +0100

    Added GitExtensions 6.x

--- gui/gitextensions.watch.py
+++ gui/gitextensions.watch.py
@@ -5,9 +5,12 @@ import github
 
 def convert(release):
     version = release['tag_name'].strip('v').replace('RC', 'rc').replace('beta', 'pre')
-    stability = 'testing' if 'rc' in version or 'pre' in version else 'stable'
-    released = release['published_at'][0:10]
-    download_url = next(asset['browser_download_url'] for asset in release['assets'] if str.endswith(asset['name'], '.msi'))
-    return {'version': version, 'download-url': download_url, 'stability': stability, 'released': released}
+    return {
+        'version': version,
+        'stability': 'testing' if 'rc' in version or 'pre' in version else 'stable',
+        'released': release['published_at'][0:10],
+        'download-url-x64':  next(asset['browser_download_url'] for asset in release['assets'] if str.startswith(asset['name'], 'GitExtensions-x64')),
+        #'download-url-arm64': next((asset['browser_download_url'] for asset in release['assets'] if str.startswith(asset['name'], 'GitExtensions-arm64')), None),
+    }
 
-releases = [convert(release) for release in github.releases('gitextensions/gitextensions') if str.startswith(release['tag_name'], 'v5') and not 'alpha' in release['tag_name']]
+releases = [convert(release) for release in github.releases('gitextensions/gitextensions') if str.startswith(release['tag_name'], 'v6') and not 'alpha' in release['tag_name']]

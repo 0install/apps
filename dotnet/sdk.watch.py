473ba06118ebd0407e4d5d89f86f4e860d53c772
Bastian Eicher <bastian@eicher.net>
2025-11-12 09:02:29 +0100

    Removed watch for outdated .NET versions

--- dotnet/sdk.watch.py
+++ dotnet/sdk.watch.py
@@ -9,4 +9,4 @@ def get_releases(channel):
         'released': release['release-date']
     } for release in json.loads(data)['releases'] if not '-' in release['release-version']]
 
-releases = get_releases('6.0') + get_releases('7.0') + get_releases('8.0') + get_releases('9.0')
+releases = get_releases('8.0') + get_releases('9.0')

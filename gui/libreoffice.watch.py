8dbad114874890a4d40e4ee3eed9141d1dbf82b8
Bastian Eicher <bastian@eicher.net>
2026-01-22 09:19:59 +0100

    Skip broken LibreOffice version

--- gui/libreoffice.watch.py
+++ gui/libreoffice.watch.py
@@ -3,7 +3,7 @@ from urllib import request
 import re
 from datetime import datetime
 
-excluded_versions = ['24.8.1.2']
+excluded_versions = ['24.8.1.2', '26.2.0.2']
 
 data = request.urlopen('https://downloadarchive.documentfoundation.org/libreoffice/old/').read().decode('utf-8')
 matches = re.findall(r'>([0-9\.]+)\/<\/a><\/td><td align="right">(..-...-....)', data)
@@ -12,3 +12,4 @@ releases = [{
     'version-original': match[0],
     'released': datetime.strftime(datetime.strptime(match[1], '%d-%b-%Y'), '%Y-%m-%d')
 } for match in matches if not (match[0].startswith("3.") or match[0].startswith("4.") or match[0].startswith("5.") or match[0] in excluded_versions)]
+

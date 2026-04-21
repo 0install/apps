bb2c757d590e5e57d8ccb04914bd5abaf1d901fb
Bastian Eicher <bastian@eicher.net>
2026-04-21 21:45:56 +0200

    Fixed VS Code watch script

--- gui/vs-code.watch.py
+++ gui/vs-code.watch.py
@@ -2,9 +2,7 @@ import sys, os
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import github
 
-excluded_versions = ['v0.44.1']
-
 releases = [{
     'version': release['tag_name'].strip('v'),
     'released': release['published_at'][0:10]
-} for release in github.releases('microsoft/vscode') if release['tag_name'] not in excluded_versions]
+} for release in github.releases('microsoft/vscode') if not str.startswith(release['name'], 'copilot/')]

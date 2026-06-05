493cc45cda41021b71540fa21ecc51c002a21c48
Bastian Eicher <bastian@eicher.net>
2026-06-05 09:11:04 +0200

    Added chezmoi

---
+++ utils/chezmoi.watch.py
@@ -0,0 +1,9 @@
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import github
+
+releases = [{
+    'version': release['tag_name'].lstrip('v'),
+    'released': release['published_at'][0:10]
+} for release in github.releases('twpayne/chezmoi')
+   if not release['prerelease'] and release['assets']]

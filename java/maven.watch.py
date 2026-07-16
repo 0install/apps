e20c572964403dc371f74d96a65710cdda754224
Bastian Eicher <bastian@eicher.net>
2026-07-17 00:05:31 +0200

    Updated Maven

--- java/maven.watch.py
+++ java/maven.watch.py
@@ -2,10 +2,10 @@ from urllib import request
 import re
 
 data = request.urlopen('http://archive.apache.org/dist/maven/maven-3/').read().decode('utf-8')
-matches = re.findall(r'([0-9\.\-alphabeta]+)\/<\/a>\s+(....-..-..)', data)
+matches = re.findall(r'([0-9\.\-alphabetarc]+)\/<\/a>\s+(....-..-..)', data)
 releases = [{
     'version': match[0].replace('alpha', 'pre1').replace('beta', 'pre2'),
     'version-original': match[0],
-    'stability': 'developer' if 'alpha' in match[0] else ('testing' if 'beta' in match[0] else 'stable'),
+    'stability': 'developer' if 'alpha' in match[0] else ('testing' if ('beta' in match[0] or 'rc' in match[0]) else 'stable'),
     'released': match[1]
 } for match in matches]

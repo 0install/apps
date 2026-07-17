53473292857e63e01cd04bd08312346d52170e11
Bastian Eicher <bastian@eicher.net>
2026-07-17 14:03:20 +0200

    Added grpc-health-probe

---
+++ protobuf/grpc-health-probe.watch.py
@@ -0,0 +1,8 @@
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import github
+
+releases = [{
+    'version': release['tag_name'].lstrip('v'),
+    'released': release['published_at'][0:10]
+} for release in github.releases('grpc-ecosystem/grpc-health-probe') if not release['prerelease'] and release['assets']]

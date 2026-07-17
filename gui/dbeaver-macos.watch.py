afa187b544308dee71e1436b81bd770f5f86d60c
Bastian Eicher <bastian@eicher.net>
2026-07-17 13:37:53 +0200

    Added DBeaver

---
+++ gui/dbeaver-macos.watch.py
@@ -0,0 +1,10 @@
+#os=Darwin
+import sys, os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+import github
+
+releases = [{
+    'version': release['tag_name'],
+    'released': release['published_at'][0:10],
+} for release in github.releases('dbeaver/dbeaver')
+   if not release['prerelease'] and any(asset['name'].endswith('-macos-x86_64.dmg') for asset in release['assets'])]

d05c120adeed8b5ba88bca51baf81f94f8b67652
Bastian Eicher <bastian@eicher.net>
2026-07-17 16:05:37 +0200

    Updated protoc

--- protobuf/protoc.watch.py
+++ protobuf/protoc.watch.py
@@ -1,12 +1,17 @@
 #os=Linux
-import sys, os
+import sys, os, re
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 import github
 
 def convert(release):
     tag = release['tag_name']
     version = tag.strip('v').replace('rc.', 'rc').replace('rc', '-rc').replace('--', '-')
-    archive_version = version.replace('rc', 'rc-')
+    protoc_asset = next((a['name'] for a in release['assets'] if a['name'].startswith('protoc-')), None)
+    if protoc_asset:
+        m = re.match(r'protoc-(.+?)-(linux|osx|win)', protoc_asset)
+        archive_version = m.group(1) if m else version.replace('rc', 'rc-')
+    else:
+        archive_version = version.replace('rc', 'rc-')
     stability = 'testing' if release['prerelease'] else 'stable'
     released = release['published_at'][0:10]
     return {'tag': tag, 'version': version, 'archive-version': archive_version, 'stability': stability, 'released': released}

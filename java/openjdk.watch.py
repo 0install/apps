907df404bba448ea4478ec75e7def652663cfa8b
Bastian Eicher <bastian@eicher.net>
2026-09-16 13:07:01 +0200

    Updated OpenJDK

--- java/openjdk.watch.py
+++ java/openjdk.watch.py
@@ -1,6 +1,9 @@
 from urllib import request
 import re
 
-data = request.urlopen('http://jdk.java.net/26/').read().decode('utf-8')
+index = request.urlopen('https://jdk.java.net/').read().decode('utf-8')
+major = re.search(r'GA Releases.*?<a href="\./(\d+)/">JDK \1</a>', index, re.DOTALL).group(1)
+
+data = request.urlopen(f'https://jdk.java.net/{major}/').read().decode('utf-8')
 matches = re.findall(r'GA\/jdk([0-9\.]+)\/([0-9a-f]+\/[0-9]+)\/', data)[0]
 releases = [{'version': matches[0], 'release-id': matches[1]}]
